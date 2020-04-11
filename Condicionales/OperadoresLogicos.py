c493a1f09364fde9a6934bf4501c873a58f87a31
BrianMarquez3 <brian3marquez@gmail.com>
2020-04-10 19:02:45 -0500

    Tema 5 - Operadores logicos completo

--- Condicionales/OperadoresLogicos.py
+++ Condicionales/OperadoresLogicos.py
@@ -9,8 +9,16 @@ x = int(input("ingrese un numero: "))
 y = int(input("ingrese un numero: "))
 
 if x > 2 and y <= 10:
-    print ("1 - x es mayor que 2 y x es menor igual 10 ")
+    print ("1 opcion - x es mayor que 2 y x es menor igual 10 ")
+else:
+    print ("no se puede ejecutar la 1 opcion")
+
 if x > 2 or y <= 10:
-    print ("2 - x es mayor que 2 o x es menor igual que 10")
+    print ("2 Opcion - x es mayor que 2 o x es menor igual que 10")
+else:
+    print ("no se puede ejecutar la 2 opcion")
+
 if (not( x==y )):
-    print("3 - x no es igual que y")
+    print("3 opcion - x no es igual que y")
+else:
+    print ("no se puede ejecutar la 3 opcion")
