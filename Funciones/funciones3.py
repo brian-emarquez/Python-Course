5b84d3fe4397ebcd57226e31c448976f083a7b41
Brian Enrique Marquez Inca Roca <brian3marquez@outlook.com>
2020-06-09 20:18:03 -0500

    test f

---
+++ Funciones/funciones3.py
@@ -0,0 +1,17 @@
+#funciones3
+#Operaciones Basica con Funciones
+
+def suma(numero1, numero2):
+    resultado=(numero1+numero2)
+
+    return resultado
+print(suma(15,5))
+
+def suma1(numero1, numero2):
+    resultado=(numero1+numero2)
+
+    return resultado
+
+suma1=(suma(15,5))
+print(suma)
+    
