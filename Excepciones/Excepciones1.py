ebc1633dc998109385543b972dbc9da8749f077b
Brian Enrique Marquez Inca Roca <brian3marquez@outlook.com>
2020-06-16 21:29:15 -0500

    Excepciones 1

---
+++ Excepciones/Excepciones1.py
@@ -0,0 +1,40 @@
+# Excepciones
+def suma(num1, num2):
+	return num1+num2
+
+def resta(num1, num2):
+	return num1-num2
+
+def multiplica(num1, num2):
+	return num1*num2
+
+def divide(num1,num2):	
+	try:	
+		return num1/num2
+	except ZeroDivisionError:
+		print("No se puede Dividir entre 0")
+
+op1=(int(input("Introduce el primer numero: ")))
+
+op2=(int(input("Introduce el segundo numero: ")))		
+	
+operacion=input("Introduce la operacion a realizar (suma,resta,multiplica,divide): ")
+
+if operacion=="suma":
+	print(suma(op1,op2))
+
+elif operacion=="resta":
+	print(resta(op1,op2))
+
+elif operacion=="multiplica":
+	print(multiplica(op1,op2))
+
+elif operacion=="divide":
+	print(divide(op1,op2))
+
+else:
+	print ("Operacion no contemplada")
+
+
+print("Operacion ejecutada. Continuacion de ejecucion del programa ")
+
