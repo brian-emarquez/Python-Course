39f644058c957e21c71a5334ea73a8e99f68440b
BrianMarquez3 <brian3marquez@gmail.com>
2020-06-30 20:06:44 -0500

    Paquetes

---
+++ Paquetes/Calculos/OperacionBasica/Operacion_Basica.py
@@ -0,0 +1,13 @@
+
+
+def sumar(op1, op2):
+    print("El resulrado de la suma es: ", op1+op2)
+
+def restar(op1, op2):
+    print("El resulrado de la resta es: ", op1-op2)
+
+def multiplicar(op1, op2):
+    print("El resulrado de la multipliacion es: ", op1*op2)
+
+def dividir(dividendo, divisor):
+    print("El resulrado de la division es: ", dividendo/divisor)
