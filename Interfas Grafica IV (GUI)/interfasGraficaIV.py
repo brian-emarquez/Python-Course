e121b6c1bcf763cee2858ecb64148b973f6592bc
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-06 16:40:14 -0500

    Interfas Grafica V

---
+++ Interfas Grafica IV (GUI)/interfasGraficaIV.py
@@ -0,0 +1,33 @@
+# Interfas Grafica IV
+# Entry - Entrada de datos
+# Grid
+#padx,y
+
+from tkinter import *
+
+raiz = Tk()
+
+miFrame = Frame(raiz, width=1200, height=600)
+miFrame.pack()
+
+cuadroNombre=Entry(miFrame)
+cuadroNombre.grid(row=0, column=1, padx=10, pady=10)
+cuadroNombre.config(fg="red", justify="center")
+
+cuadroApellido=Entry(miFrame)
+cuadroApellido.grid(row=1, column=1, padx=10, pady=10)
+
+cuadroDireccion=Entry(miFrame)
+cuadroDireccion.grid(row=2, column=1, padx=10, pady=10)
+
+#Label
+nombreLabel=Label(miFrame, text="Nombre:")
+nombreLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+nonmbreApellido=Label(miFrame, text="Apellido:")
+nonmbreApellido.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+direccionLabel=Label(miFrame, text="Direccion de la casa:")
+direccionLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+
+raiz.mainloop()
