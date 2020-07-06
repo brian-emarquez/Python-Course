b6eca84b0cc3cec74d7433f5e6458eebf046f6c4
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-06 16:47:18 -0500

    Update

--- Interfas Grafica IV (GUI)/interfasGraficaIV.py
+++ Interfas Grafica IV (GUI)/interfasGraficaIV.py
@@ -14,20 +14,28 @@ cuadroNombre=Entry(miFrame)
 cuadroNombre.grid(row=0, column=1, padx=10, pady=10)
 cuadroNombre.config(fg="red", justify="center")
 
+cuadroPass=Entry(miFrame)
+cuadroPass.grid(row=1, column=1, padx=10, pady=10)
+cuadroPass.config(show="*")
+
+
 cuadroApellido=Entry(miFrame)
-cuadroApellido.grid(row=1, column=1, padx=10, pady=10)
+cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
 
 cuadroDireccion=Entry(miFrame)
-cuadroDireccion.grid(row=2, column=1, padx=10, pady=10)
+cuadroDireccion.grid(row=3, column=1, padx=10, pady=10)
 
 #Label
 nombreLabel=Label(miFrame, text="Nombre:")
 nombreLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
 
-nonmbreApellido=Label(miFrame, text="Apellido:")
-nonmbreApellido.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+nonmbrePass=Label(miFrame, text="Password:")
+nonmbrePass.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 
-direccionLabel=Label(miFrame, text="Direccion de la casa:")
-direccionLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+nonmbreApellido=Label(miFrame, text="Apellido:")
+nonmbreApellido.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+
+direccionDireccion=Label(miFrame, text="Direccion de la casa:")
+direccionDireccion.grid(row=3, column=0, sticky="e", padx=10, pady=10)
 
 raiz.mainloop()
