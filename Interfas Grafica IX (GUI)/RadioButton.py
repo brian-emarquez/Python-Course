da5a51de1c6f60b4f1abea1891672bd9cb01c58a
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-10 16:40:51 -0500

    Radio Button

---
+++ Interfas Grafica IX (GUI)/RadioButton.py
@@ -0,0 +1,27 @@
+# Interfas Grafica IX 
+# Raddio Button
+
+
+from tkinter import *
+
+root=Tk()
+varOpcion=IntVar()
+
+
+def imprimir():
+    #print(varOpcion.get())
+    if varOpcion.get()==1:
+        etiqueta.config(text="Has elegido masculino")
+
+    else: 
+        etiqueta.config(text="Has elegido femenino")
+
+Label(root, text="Genero:").pack()
+
+Radiobutton(root, text="Masculino", variable= varOpcion, value=1, comman=imprimir).pack()
+Radiobutton(root, text="Femenino", variable= varOpcion, value=2, comman=imprimir).pack()
+
+etiqueta=Label(root)
+etiqueta.pack()
+
+root.mainloop()
