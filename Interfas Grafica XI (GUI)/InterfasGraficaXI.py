e760e7228b695b191e51202c40e4ec423eaf4368
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-13 20:50:23 -0500

    Menu update

--- Interfas Grafica XI (GUI)/InterfasGraficaXI.py
+++ Interfas Grafica XI (GUI)/InterfasGraficaXI.py
@@ -7,24 +7,30 @@ from tkinter import *
 root=Tk()
 
 barraMenu=Menu(root)
-root.config(menu=barraMenu, width=300, height=300)
+root.config(menu=barraMenu, width=600, height=400)
 
-archivoMenu=Menu(barraMenu)
+archivoMenu=Menu(barraMenu, tearoff=0)
 archivoMenu.add_command(label="Nuevo")
 archivoMenu.add_command(label="Guardar")
 archivoMenu.add_command(label="Guardar Como")
+archivoMenu.add_separator()
 archivoMenu.add_command(label="Cerrar")
 archivoMenu.add_command(label="Salir")
 
-archivoEdicion=Menu(barraMenu)
-
+archivoEdicion=Menu(barraMenu, tearoff=0)
 archivoHerramientas=Menu(barraMenu)
+archivoEdicion.add_command(label="Copiar")
+archivoEdicion.add_command(label="Cortar")
+archivoEdicion.add_command(label="Pegar")
 
-archivoAyuda=Menu(barraMenu)
-
+archivoAyuda=Menu(barraMenu, tearoff=0)
 barraMenu.add_cascade(label="Archivo", menu=archivoMenu)
 barraMenu.add_cascade(label="Edicion", menu=archivoEdicion)
 barraMenu.add_cascade(label="Herramienta", menu=archivoHerramientas)
+
 barraMenu.add_cascade(label="Ayuda", menu=archivoAyuda)
+archivoAyuda.add_command(label="Licencia")
+archivoAyuda.add_command(label="Acerca de...")
+
 
 root.mainloop()
