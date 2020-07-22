324ac20e1d9ab9bdd77cf81de3aca1bec5a6e2a0
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-22 16:39:40 -0500

    Practica

--- Practica Guiada I/PracticaGuiadaI.py
+++ Practica Guiada I/PracticaGuiadaI.py
@@ -1,2 +1,33 @@
 # Aplicacion Grafica Guiada I
-#Aplicacion Grafica CRUD
+# Aplicacion Grafica CRUD
+
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+root=Tk()
+
+barraMenu=Menu(root)
+root.config(menu=barraMenu, width=300, height=300)
+
+bbddMenu=Menu(barraMenu, tearoff=0)
+bbddMenu.add_command(label="Conectar")
+bbddMenu.add_command(label="Salir")
+
+borrarMenu=Menu(barraMenu, tearoff=0)
+borrarMenu.add_command(label="Borrar Campos")
+
+crudMenu=Menu(barraMenu, tearoff=0)
+crudMenu.add_command(label="Crear")
+crudMenu.add_command(label="Leer")
+crudMenu.add_command(label="Actualizar")
+crudMenu.add_command(label="Borrar")
+
+ayudaMenu=Menu(barraMenu, tearoff=0)
+ayudaMenu.add_command(label="Licencia")
+ayudaMenu.add_command(label="Acerca de")
+
+
+
+root.mainloop()
+
