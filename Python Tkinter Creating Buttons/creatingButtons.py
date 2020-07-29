bd0b493aeec2e7a6fa3c6225720801907cef4f1c
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-29 14:00:41 -0500

    Python Tkinter Creating Buttons

---
+++ Python Tkinter Creating Buttons/creatingButtons.py
@@ -0,0 +1,14 @@
+# Python Tkinter Creating Buttons
+from tkinter import *
+root=  Tk()
+
+def myClick():
+    myLabel = Label(root, text= "Look i click a button!")
+    myLabel.pack()
+
+
+
+myButton = Button(root, text="Click Me!", padx=50, command=myClick)
+myButton.pack()
+
+root.mainloop()
