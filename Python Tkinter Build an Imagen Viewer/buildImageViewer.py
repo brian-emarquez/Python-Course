53ca669c7c81e6d1e26649d2644cf082f733521a
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-31 17:24:51 -0500

    Python Tkinter Build an Imagen Viewer

--- Python Tkinter Build an Imagen Viewer/buildImageViewer.py
+++ Python Tkinter Build an Imagen Viewer/buildImageViewer.py
@@ -33,19 +33,34 @@ def forward(image_number):
     Button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
     Button_back = Button(root, text="<<", command=lambda: back(image_number-1))
 
+    if image_number == 6:
+        Button_forward = Button(root, text=">>", state=DISABLED )
+
+
     my_label.grid(row=0, column=0, columnspan=3)
     Button_back.grid(row=1, column=0)
     Button_forward.grid(row=1, column=2)
 
 
-def back():
+def back(image_number):
     global my_label
     global Button_forward
     global Button_back
 
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    Button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    Button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 1:
+        Button_back = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    Button_back.grid(row=1, column=0)
+    Button_forward.grid(row=1, column=2)
 
 
-Button_back = Button(root, text="<<", command = lambda: back())
+Button_back = Button(root, text="<<", command = lambda: back, state=DISABLED) 
 Button_exit = Button(root, text="EXIT PROGRAM", command = root.quit)
 Button_forward = Button(root, text=">>", command = lambda: forward(2))
 
