085563849624635bac6b5a425f8a9fe05b03e667
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-01 12:52:52 -0500

    Pyhton Tkinter Create New Window

--- Pyhton Tkinter Create New Windows/createNewWindows.py
+++ Pyhton Tkinter Create New Windows/createNewWindows.py
@@ -8,4 +8,17 @@ root = Tk()
 root.title('Learn to Python')
 root.iconbitmap('Pyhton Tkinter Create New Windows/icon.ico')
 
+def open():
+    global my_img
+
+    top = Toplevel() # abre 2 ventanas
+    top.title("My Second Windows")
+    top.iconbitmap('Pyhton Tkinter Create New Windows/icon.ico')
+    my_img = ImageTk.PhotoImage(Image.open("Pyhton Tkinter Create New Windows/image.jpg"))
+    my_label = Label(top, image=my_img).pack()
+    btn2= Button(top, text="Close Window", command=top.destroy).pack()
+
+btn = Button(root, text="Open Second windos", command=open).pack()
+
+
 root.mainloop()
