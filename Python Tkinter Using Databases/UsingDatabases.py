7b15ba63965b97158943478873716077adca852d
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-03 16:30:13 -0500

    Python Tkinter Using Databases Start

---
+++ Python Tkinter Using Databases/UsingDatabases.py
@@ -0,0 +1,13 @@
+# Python Tkinter Using Databases
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Learn to Tkinter')
+root.iconbitmap('Python Tkinter Using Databases/db.ico')
+root.geometry("400x400") 
+
+
+
+root.mainloop()
