e9180e7a82f4d784e4a4d02752ab4611377283a7
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-03 15:57:52 -0500

    Pyhton Tkinter Dropdown Menus start

---
+++ Python Tkinter Dropdown Menus/dropdownMenus.py
@@ -0,0 +1,12 @@
+# Python Tkinter Dropdown Menus
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Learn to Tkinter')
+root.iconbitmap('Python Tkinter Dropdown Menus/icon.ico')
+root.geometry("400x400") 
+
+
+root.mainloop()
