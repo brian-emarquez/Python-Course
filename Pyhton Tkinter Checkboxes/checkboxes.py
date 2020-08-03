e8e885a0927dfb08fa2b0b2e61bc055cfcb74ab2
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-03 15:46:20 -0500

    Python Tkinter Checkboxes

--- Pyhton Tkinter Checkboxes/checkboxes.py
+++ Pyhton Tkinter Checkboxes/checkboxes.py
@@ -5,5 +5,18 @@ from PIL import ImageTk, Image
 
 root = Tk()
 root.title('Learn to Tkinter')
+root.iconbitmap('Pyhton Tkinter Checkboxes/icon.ico')
+root.geometry("400x400") 
+# Funciones
+def show():
+    myLabel = Label(root, text=var.get()).pack()
+
+var = StringVar()
+c = Checkbutton(root, text="Check this box. I dare you!", variable=var, onvalue="On", offvalue="Off")
+c.deselect() # Seleccion del del checkBoxs
+c.pack()
+
+myLabel = Label(root, text=var.get()).pack()
+myButton = Button(root, text="Show selection", command=show).pack()
 
 root.mainloop()
