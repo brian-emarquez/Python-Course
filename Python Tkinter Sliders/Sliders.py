97b71389d8542d7b9c646b1bb780c85807d53de3
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-03 11:46:01 -0500

    Python tkinter Sliders - Star

---
+++ Python Tkinter Sliders/Sliders.py
@@ -0,0 +1,12 @@
+# Python Tkinter Sliders
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Learn to Python')
+root.iconbitmap('Python Tkinter Radio Butttons/icon.ico')
+
+
+
+root.mainloop()
