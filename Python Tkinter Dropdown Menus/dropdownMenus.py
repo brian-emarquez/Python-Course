5f9898cb33420c1e7003faeaffc861e6cf549952
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-03 16:18:33 -0500

    Python Tkinter Dropdown Menus

--- Python Tkinter Dropdown Menus/dropdownMenus.py
+++ Python Tkinter Dropdown Menus/dropdownMenus.py
@@ -8,5 +8,24 @@ root.title('Learn to Tkinter')
 root.iconbitmap('Python Tkinter Dropdown Menus/icon.ico')
 root.geometry("400x400") 
 
+# Drop down Boxes
+def show():
+    myLabel = Label(root, text=clicked.get()).pack()
+
+options = [
+    "Lunes", 
+    "Martes", 
+    "Miercoles",
+    "Jueves", 
+    "Viernes"
+]
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop = OptionMenu(root, clicked, *options)
+drop.pack()
+
+myButton = Button(root, text="Show Selection", command=show).pack()
 
 root.mainloop()
