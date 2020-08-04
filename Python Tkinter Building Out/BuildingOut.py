b0f67f23aaac98cbf74e2ac86e1ee0daab421861
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-04 11:17:31 -0500

    Pyton Tkinter Building Out Start

---
+++ Python Tkinter Building Out/BuildingOut.py
@@ -0,0 +1,14 @@
+# Python Tkinter 
+
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Pyhton Image Viewer') # titulo
+root.iconbitmap('Python Tkinter Building Out/db.ico')
+root.geometry("400x400") 
+
+
+
+root.mainloop()
