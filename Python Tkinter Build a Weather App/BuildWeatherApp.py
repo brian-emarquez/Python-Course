9e5367ae3d58db971ed359dc29cd1b3f5488f65e
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-06 15:59:22 -0500

    Python Tkinter Update a Record II Requests

--- Python Tkinter Build a Weather App/BuildWeatherApp.py
+++ Python Tkinter Build a Weather App/BuildWeatherApp.py
@@ -2,12 +2,16 @@
 
 from tkinter import *
 from PIL import ImageTk, Image
-import sqlite3
+import requests
+import json
 
 root = Tk()
 root.title('Python Tkinter Build a Weather App')
 root.iconbitmap('Python Tkinter Build a Weather App/weather.ico')
-root.geometry("400x400") 
+root.geometry("400x50") 
+
+# http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=D3E1D2B8-09F8-4FB3-AB63-6A2B716BEF52
+
 
 
 root.mainloop()
