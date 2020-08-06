a2fd861611b9897971c32cd3af28fa4feccf12c8
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-06 18:34:06 -0500

    Pyhton Tkinter Add ZipCode start

---
+++ Python Tkinter Matplolib Charts/MatplolibCharts.py
@@ -0,0 +1,12 @@
+# Pyhton Tkinter Add ZipCode
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Pyhton Tkinter Add ZipCode')
+#root.iconbitmap('Pyhton Tkinter Add ZipCode/weather.ico')
+root.geometry("400x400") 
+
+
+root.mainloop()
