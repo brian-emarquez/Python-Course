66f266eccd0bc93607436d18468bfdabc8a0bef7
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-05 21:19:19 -0500

    Python Tkinter Update a Record II start

---
+++ Python Tkinter Build a Weather App/BuildWeatherApp.py
@@ -0,0 +1,13 @@
+# Python Tkinter Update a Record II
+
+from tkinter import *
+from PIL import ImageTk, Image
+import sqlite3
+
+root = Tk()
+root.title('Python Tkinter Build a Weather App')
+root.iconbitmap('Python Tkinter Build a Weather App/weather.ico')
+root.geometry("400x400") 
+
+
+root.mainloop()
