0110cc2b3282f3ee352a91d8001665d9f8698bdd
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-07 16:24:00 -0500

    Python Tkinter Matplolib Charts

--- Python Tkinter Matplolib Charts/MatplolibCharts.py
+++ Python Tkinter Matplolib Charts/MatplolibCharts.py
@@ -1,12 +1,27 @@
-# Pyhton Tkinter Add ZipCode
+# Python Tkinter Matplolib Charts
+# numpy
 
 from tkinter import *
 from PIL import ImageTk, Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 root = Tk()
-root.title('Pyhton Tkinter Add ZipCode')
-#root.iconbitmap('Pyhton Tkinter Add ZipCode/weather.ico')
-root.geometry("400x400") 
+root.title('Python Tkinter Matplolib Charts')
+root.iconbitmap('Python Tkinter Matplolib Charts/check.ico')
+root.geometry("400x200") 
+
+
+def graph():
+    house_prices = np.random.normal(200000, 25000, 5000) 
+    #plt.pie(house_prices, 200) # tipo de grafico, granularidad
+    #plt.pie(house_prices) 
+    plt.polar(house_prices) 
+    plt.show()
+
+my_button = Button(root, text="Graph It!", command=graph)
+my_button.pack()
+
 
 
 root.mainloop()
