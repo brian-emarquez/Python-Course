dc6fc6b78527f689ea980e7152a42217d8e3fd99
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-10 17:54:29 -0500

    ython Tkinter Create CRM database Tool

--- Python Tkinter Create CRM database Tool/createCRMdatabaseTool.py
+++ Python Tkinter Create CRM database Tool/createCRMdatabaseTool.py
@@ -3,25 +3,20 @@
 
 from tkinter import *
 from PIL import ImageTk, Image
-import numpy as np
-import matplotlib.pyplot as plt
+import mysql.connector
 
 root = Tk()
 root.title('Python Tkinter Create CRM database Tool')
 root.iconbitmap('Python Tkinter Create CRM database Tool/check.ico')
 root.geometry("400x200") 
 
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  port = 3308,
+  user="briandb",
+  password="briandb"
+)
 
-def graph():
-    house_prices = np.random.normal(200000, 25000, 5000) 
-    #plt.pie(house_prices, 200) # tipo de grafico, granularidad
-    #plt.pie(house_prices) 
-    plt.polar(house_prices) 
-    plt.show()
-
-my_button = Button(root, text="Graph It!", command=graph)
-my_button.pack()
-
-
+print(mydb)
 
 root.mainloop()
