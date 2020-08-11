36f942f8d2de9920b53a61ed90e0e8e4663d92d1
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-10 19:02:35 -0500

    Python Tkinter Create a Database and Table CRM update

--- Python Tkinter Create a Database and Table CRM/createDatabaseTableCRM.py
+++ Python Tkinter Create a Database and Table CRM/createDatabaseTableCRM.py
@@ -15,7 +15,8 @@ mydb = mysql.connector.connect(
     host="127.0.0.1",
     port = 3308,
     user="briandb",
-    password="briandb"
+    password="briandb",
+    #database = "db_python",
 )
 
 #Chack to see if connection to MYSQL was created
@@ -26,5 +27,13 @@ my_cursor = mydb.cursor()
 
 # Create Database
 my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
+
+# Create Table
+my_cursor.execute("CREATE TABLE customers (first_name VARCHAR(255), last_name VARCHAR(255), zipcade INT(10), price_paid DECIMAL(10, 2), user_id INT AUTO_INCREMENT PRIMARY KEY)")
+
+#my_cursor.execute("SELECT * FROM customers")
+#print(my_cursor.description)
 
 root.mainloop()
