18547da4564afd5ef9aa3bb47956480deaffa7ce
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-12 14:07:24 -0500

    Python Tkinter MariaDB Correct

--- Python Tkinter MariaDB/mariadb.py
+++ Python Tkinter MariaDB/mariadb.py
@@ -1,9 +1,10 @@
 # Python Tkinter MariaDB
 
 from tkinter import *
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image
 #import mysql.connector
-import mariadb
+import mysql.connector as mariadb
+
 import sys
 
 root = Tk()
@@ -11,15 +12,15 @@ root.title('Python Tkinter MariaDB')
 root.iconbitmap('Python Tkinter MariaDB/check.ico')
 root.geometry("400x200") 
 
-mariadb_conexion = mariadb.connect(
+conn = mariadb.connect(
     host='127.0.0.1',
     port=3307, 
     user='briandb', 
     password='briandb'
-    #tabase='test2'
 )
 
-print(mariadb_conexion)
+print(conn)
+conn.close()
 root.mainloop()
 
 
