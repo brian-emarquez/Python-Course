12feeb5e720e344e3523054b804d686958d69b39
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-18 12:52:27 -0500

    Python Tkinter Update MariaDb records update 3

--- Python Tkinter Update MariaDb records/Update MariaDb records.py
+++ Python Tkinter Update MariaDb records/Update MariaDb records.py
@@ -98,7 +98,6 @@ zipcade_label = Label(root, text="Zipcode").grid(row=7, column=0)
 country_label = Label(root, text="Country").grid(row=8, column=0)
 phone_label = Label(root, text="Phone Number").grid(row=9, column=0)
 email_label = Label(root, text="Email Address").grid(row=10, column=0)
-#username_label = Label(root, text="Username").grid(row=11, column=0)
 payment_method_label = Label(root, text="Payment Method").grid(row=11, column=0)
 dicount_code_label = Label(root, text="Discount Code").grid(row=12, column=0)
 price_paid_label = Label(root, text="Price Paid").grid(row=13, column=0)
@@ -125,6 +124,62 @@ def search_customer():
     search_customers.title("Search All Customers")
     search_customers.iconbitmap('Python Tkinter Update MariaDb records/db.ico')
     search_customers.geometry("1100x600") 
+
+    def edit_now(id, row):
+    
+    first_name_label = Label(root, text="First Name").grid(row=1, column=0)
+    last_name_label = Label(root, text="Last Name").grid(row=2, column=0)
+    address_1_label = Label(root, text="Address 1").grid(row=3, column=0)
+    address_2_label = Label(root, text="Address 2").grid(row=4, column=0)
+    city_label = Label(root, text="City").grid(row=5, column=0)
+    state_label = Label(root, text="State").grid(row=6, column=0)
+    zipcade_label = Label(root, text="Zipcode").grid(row=7, column=0)
+    country_label = Label(root, text="Country").grid(row=8, column=0)
+    phone_label = Label(root, text="Phone Number").grid(row=9, column=0)
+    email_label = Label(root, text="Email Address").grid(row=10, column=0)
+    payment_method_label = Label(root, text="Payment Method").grid(row=11, column=0)
+    dicount_code_label = Label(root, text="Discount Code").grid(row=12, column=0)
+    price_paid_label = Label(root, text="Price Paid").grid(row=13, column=0)
+
+    # Create Entry Boxes
+    first_name_box = Entry(root)
+    first_name_box.grid(row=1, column=1)
+
+    last_name_box = Entry(root)
+    last_name_box.grid(row=2, column=1, pady=5)
+
+    address_1_box = Entry(root)
+    address_1_box.grid(row=3, column=1, pady=5)
+
+    address_2_box = Entry(root)
+    address_2_box.grid(row=4, column=1, pady=5)
+
+    city_box = Entry(root)
+    city_box.grid(row=5, column=1, pady=5)
+
+    state_box = Entry(root)
+    state_box.grid(row=6, column=1, pady=5)
+
+    zipcade_box = Entry(root)
+    zipcade_box.grid(row=7, column=1, pady=5)
+
+    country_box = Entry(root)
+    country_box.grid(row=8, column=1, pady=5)
+
+    phone_box = Entry(root)
+    phone_box.grid(row=9, column=1, pady=5)
+
+    email_box = Entry(root)
+    email_box.grid(row=10, column=1, pady=5)
+
+    payment_method_box = Entry(root)
+    payment_method_box.grid(row=11, column=1, pady=5)
+
+    dicount_cod_box = Entry(root)
+    dicount_cod_box.grid(row=12, column=1, pady=5)
+
+    price_paid_box = Entry(root)
+    price_paid_box.grid(row=13, column=1, pady=5)
 
     def seach_now():
 
@@ -156,7 +211,8 @@ def search_customer():
             for index, x in enumerate(result):
                 num = 0
                 index +=2
-                edit_button= Button(search_customers, text="Edit")
+                id_reference = str(x[4])
+                edit_button= Button(search_customers, text="Edit ", command=Lambda:edit_now(id_reference, index))
                 edit_button.grid(row=index, column=num)
 
                 for y in x:
