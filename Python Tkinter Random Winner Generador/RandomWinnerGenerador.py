e712c07e9f91fed00915108575671ffd9d3961b5
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-18 22:08:54 -0500

    Python Tkinter Random Winner Generador start

---
+++ Python Tkinter Random Winner Generador/RandomWinnerGenerador.py
@@ -0,0 +1,16 @@
+# Python Tkinter Random Winner Generador
+# Generador de ganador aleatorio de Python Tkinter
+
+
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Random Winner Generador!')
+root.iconbitmap('Python Tkinter Random Winner Generador/umbrella.ico')
+root.geometry("400x600") 
+
+entries = ['Sanbid Ray ', 'The Refuge Malik', 'Google India']
+
+
+root.mainloop()
