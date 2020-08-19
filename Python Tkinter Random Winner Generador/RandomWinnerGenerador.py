d0a064da0669d21363a4b26a7f4c8dd1eeb74937
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-19 13:42:35 -0500

    Python Tkinter Random Winner Generador

--- Python Tkinter Random Winner Generador/RandomWinnerGenerador.py
+++ Python Tkinter Random Winner Generador/RandomWinnerGenerador.py
@@ -1,8 +1,6 @@
 # Python Tkinter Random Winner Generador
 # Generador de ganador aleatorio de Python Tkinter
 
-
-
 from tkinter import *
 
 root = Tk()
@@ -10,7 +8,15 @@ root.title('Python Tkinter Random Winner Generador!')
 root.iconbitmap('Python Tkinter Random Winner Generador/umbrella.ico')
 root.geometry("400x600") 
 
-entries = ['Sanbid Ray ', 'The Refuge Malik', 'Google India']
+entries = ['Sanbid Ray ', 'The Refuge Malik', 'Google India', 'DotDotCom', 'jon Label']
 
+def pick():
+    return
+
+topLabel = Label(root, text="Win-0-Rama!", font=("Helvetica", 24))
+topLabel.pack(pady=28)
+
+winButoon = Button(root, text="PICK OUR WINNER", font=("Helvetica", 24), command=pick)
+winButoon.pack(pady=20)
 
 root.mainloop()
