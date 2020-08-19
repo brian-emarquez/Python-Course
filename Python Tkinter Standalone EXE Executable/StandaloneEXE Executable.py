fae16dac1034ad78f2ec2b4414ad89340511ac93
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-19 15:06:40 -0500

    Python Tkinter Standalone EXE Executable start

--- Python Tkinter Standalone EXE Executable/StandaloneEXE Executable.py
+++ Python Tkinter Standalone EXE Executable/StandaloneEXE Executable.py
@@ -1,12 +1,12 @@
-# Python Tkinter Standalone EXE Executable
-# Ejecutable EXE independiente de Python Tkinter
+# Python Tkinter Random Winner Generador
+# Generador de ganador aleatorio de Python Tkinter
 
 from tkinter import *
 from random import randint
 
 root = Tk()
-root.title('Python Tkinter Standalone EXE Executable!')
-root.iconbitmap('Python Tkinter Standalone EXE Executable/umbrella.ico')
+root.title('Python Tkinter Random Winner Generador!')
+root.iconbitmap('Python Tkinter Random Winner Generador/umbrella.ico')
 root.geometry("400x600") 
 
 def pick():
