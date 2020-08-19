c19f25485bf95be142f803166b760fad38091665
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-18 21:44:55 -0500

    Python Tkinter How To Riseze Entry Box height start

---
+++ Python Tkinter How To Risize Entry Box height/How To Risize Entry Box height.py
@@ -0,0 +1,12 @@
+# Python Tkinter How To Riseze Entry Box height
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How To Risize Entry Box height!')
+root.iconbitmap('Python Tkinter How To Risize Entry Box height/umbrella.ico')
+root.geometry("400x600") 
+
+
+
+root.mainloop()
