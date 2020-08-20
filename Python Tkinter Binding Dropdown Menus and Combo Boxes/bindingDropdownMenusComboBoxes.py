bbf0ac9115e5e1e2aed7fec011d2b1fec5bd2378
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-20 17:46:38 -0500

    Python Tkinter Binding Dropdown Menus and Combo Boxes

--- Python Tkinter Binding Dropdown Menus and Combo Boxes/bindingDropdownMenusComboBoxes.py
+++ Python Tkinter Binding Dropdown Menus and Combo Boxes/bindingDropdownMenusComboBoxes.py
@@ -2,12 +2,45 @@
 # Menús desplegables de enlace de Python Tkinter y cuadros combinados
 
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
-root.title('Python Tkinter Keyboard Event Binding!')
+root.title('Python Tkinter Binding Dropdown Menus and Combo Boxes!')
 root.iconbitmap('Python Tkinter Binding Dropdown Menus and Combo Boxes/reload.ico')
 root.geometry("400x400") 
 
 
+def selected(event):
+    myLabel = Label(root, text=clicked.get()).pack()
+    if clicked.get() == 'Friday':
+        myLabel = Label(root, text= "Vay! it's Friday").pack()
+    else:
+        myLabel = Label(root, text=clicked.get()).pack()
+
+def comboclick(event):
+        myLabel = Label(root, text= myCombo.get()).pack()
+
+options = [
+    "Monday",
+    "Tuesday",
+    "wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop = OptionMenu(root, clicked, *options, command = selected)
+drop.pack(pady=20)
+
+myCombo = ttk.Combobox(root, value=options)
+myCombo.current(0)
+myCombo.bind("<<ComboboxSelected>>", comboclick)
+myCombo.pack()
+
+#myButton = Button (root, text= "Select from List", command=selected)
+#myButton.pack()
 
 root.mainloop()
