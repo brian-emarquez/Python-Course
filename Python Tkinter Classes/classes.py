255d5344d80bf9a474f87037ac816d508056f84e
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-20 12:36:21 -0500

    Python Tkinter Classes

---
+++ Python Tkinter Classes/classes.py
@@ -0,0 +1,11 @@
+# Python Tkinter Classes
+# Python Tkinter sobrescribe etiquetas de cuadrícula
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Classes!')
+root.iconbitmap('Python Tkinter Classes/classes.ico')
+root.geometry("400x400") 
+
+root.mainloop()
