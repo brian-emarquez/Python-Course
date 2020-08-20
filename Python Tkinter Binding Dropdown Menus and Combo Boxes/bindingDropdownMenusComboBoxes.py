2c9cb8a37b0d7e036fd41d25d37cbe3e21a1b9db
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-20 17:07:30 -0500

    Python Tkinter Binding Dropdown Menus and Combo Boxes start

---
+++ Python Tkinter Binding Dropdown Menus and Combo Boxes/bindingDropdownMenusComboBoxes.py
@@ -0,0 +1,13 @@
+# Python Tkinter Binding Dropdown Menus and Combo Boxes
+# Menús desplegables de enlace de Python Tkinter y cuadros combinados
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Keyboard Event Binding!')
+root.iconbitmap('Python Tkinter Binding Dropdown Menus and Combo Boxes/reload.ico')
+root.geometry("400x400") 
+
+
+
+root.mainloop()
