ee601b5b779d23702b4949a7828e193236336ca7
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-20 12:31:26 -0500

    Python Tkinter Overwrite Grid Labels

--- Python Tkinter Overwrite Grid Labels/overwriteGridLabels.py
+++ Python Tkinter Overwrite Grid Labels/overwriteGridLabels.py
@@ -1,5 +1,5 @@
-# Python Tkinter Remove Labels
-# Borrar el texto
+# Python Tkinter Overwrite Grid Labels
+# Python Tkinter sobrescribe etiquetas de cuadrícula
 
 from tkinter import *
 
@@ -8,30 +8,34 @@ root.title('Python Tkinter Overwrite Grid Labels!')
 root.iconbitmap('Python Tkinter Overwrite Grid Labels/pc.ico')
 root.geometry("400x400") 
 
+myLabel = Label(root)
 
-def myDelete():
+'''def myDelete():
     
-    #myLabel.pack_forget()
-    myLabel.destroy()
+    myLabel.grid_forget()
+    #myLabel.destroy()
     myButton['state'] = NORMAL # habilitar button
-    print(myButton.winfo_exists())
+    #print(myButton.winfo_exists())
+'''
 
 def myClick():
 
     global myLabel
+    myLabel.destroy()
+
     hello = "Hello " + e.get()
     myLabel = Label(root, text=hello)
     e.delete(0, 'end')
-    myLabel.pack(pady=10)
-    myButton['state'] = DISABLED # Desabilitar Button
+    myLabel.grid(row=3, column=0, pady=10)
+    #myButton['state'] = DISABLED # Desabilitar Button
 
-e = Entry(root, width=50 , font=('Helvetica', 14))
-e.pack(padx=10, pady=10)
+e = Entry(root, width=17 , font=('Helvetica', 30))
+e.grid(row=0, column=0, padx=10, pady=10)
 
 myButton = Button(root, text="Enter Your name", command=myClick)
-myButton.pack(pady=10)
+myButton.grid(row=1, column=0, pady=10)
 
-DeleteButton = Button(root, text='Delete Text', command=myDelete)
-DeleteButton.pack(pady=10)
+#DeleteButton = Button(root, text="Delete Text", command=myDelete)
+#DeleteButton.grid(row=2, column=0, pady=10)
 
 root.mainloop()
