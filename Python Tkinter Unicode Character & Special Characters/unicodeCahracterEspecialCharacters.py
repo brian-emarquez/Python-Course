083be205ea6e23a3a19f0a0ff0cd9d14d5a8d760
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-22 15:42:53 -0500

    Python Tkinter Unicode Character & Special Characters start

---
+++ Python Tkinter Unicode Character & Special Characters/unicodeCahracterEspecialCharacters.py
@@ -0,0 +1,11 @@
+# Python Tkinter Unicode Character & Special Characters
+# Caracteres especiales y caracteres Unicode de Python Tkinter
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Unicode Character & Special Characters!')
+root.iconbitmap('Python Tkinter Unicode Character & Special Characters/avatar.ico')
+root.geometry("400x400")
+
+root.mainloop()
