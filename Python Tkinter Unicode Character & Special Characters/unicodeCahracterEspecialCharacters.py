a69fa6fadaa61f73e611a3b2e261e31e3160f0c2
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-22 16:12:40 -0500

    Python Tkinter Unicode Character & Special Characters

--- Python Tkinter Unicode Character & Special Characters/unicodeCahracterEspecialCharacters.py
+++ Python Tkinter Unicode Character & Special Characters/unicodeCahracterEspecialCharacters.py
@@ -8,4 +8,9 @@ root.title('Python Tkinter Unicode Character & Special Characters!')
 root.iconbitmap('Python Tkinter Unicode Character & Special Characters/avatar.ico')
 root.geometry("400x400")
 
+my_label = Label(root, text='41' + u'\u00b0', font=("Helvetica", 32)).pack(pady=10)
+my_button = Button(root, text=u'\u00AB', font=("Helvetica", 32)).pack(pady=10)
+my_button1 = Button(root, text=u'\u00BB', font=("Helvetica", 32)).pack(pady=10)
+
+
 root.mainloop()
