522cde1f18282b4b97ceba4474a504734e448edc
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-22 14:06:09 -0500

    Python Tkinter Color Picker

--- Python Tkinter Color Picker/colorPicker.py
+++ Python Tkinter Color Picker/colorPicker.py
@@ -2,11 +2,19 @@
 # Selector de color de Python Tkinter
 
 from tkinter import *
+from tkinter import colorchooser
 
 
 root = Tk()
 root.title('Python Tkinter Color Picker!')
+root.geometry("400x400")
 root.iconbitmap('Python Tkinter Color Picker/avatar.ico')
-root.geometry("400x400") 
+
+def color():
+    my_color = colorchooser.askcolor()[1]
+    my_label = Label(root, text=my_color).pack(pady=10)
+    my_label = Label(root, text="Ypu Picked a Color!", font=("Helvetica", 32), bg=my_color).pack()
+
+my_button = Button(root, text="Pick A Color", command=color).pack()
 
 root.mainloop()
