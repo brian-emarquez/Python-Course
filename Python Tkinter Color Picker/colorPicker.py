e243ac3e669b952860ed2711ed60702176447a03
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-22 13:11:28 -0500

    Python Tkinter Color Picker update

--- Python Tkinter Color Picker/colorPicker.py
+++ Python Tkinter Color Picker/colorPicker.py
@@ -20,10 +20,11 @@ panel_1.add(left_label)
 panel_2 = PanedWindow(panel_1, orient=VERTICAL, bd=4, relief="raised", bg="blue")
 panel_1.add(panel_2)
 
-top = Label(panel_2, text="Top Panel")
-panel_2.add(top)
+    # heat
+    top = Label(panel_2, text="Top Panel")
+    panel_2.add(top)
 
-buton = Label(panel_2, text="Boton Panel")
-panel_2.add(buton)
+    buton = Label(panel_2, text="Boton Panel")
+    panel_2.add(buton)
 
 root.mainloop()
