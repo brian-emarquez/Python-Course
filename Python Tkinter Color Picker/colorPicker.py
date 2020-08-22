05f2c9f30f79bd8db7c8a49360fe9f1baaed2544
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-22 13:07:28 -0500

    Python Tkinter Color Picker start

---
+++ Python Tkinter Color Picker/colorPicker.py
@@ -0,0 +1,29 @@
+# Python Tkinter Color Picker
+# Selector de color de Python Tkinter
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter Color Picker!')
+root.iconbitmap('Python Tkinter Color Picker/menu.ico')
+root.geometry("400x400") 
+
+# Panel
+panel_1 = PanedWindow(bd=4, relief="raised", bg="red")
+panel_1.pack(fill=BOTH, expand=1)
+
+left_label = Label(panel_1, text="Left Panel")
+panel_1.add(left_label)
+
+# Second Panel
+panel_2 = PanedWindow(panel_1, orient=VERTICAL, bd=4, relief="raised", bg="blue")
+panel_1.add(panel_2)
+
+top = Label(panel_2, text="Top Panel")
+panel_2.add(top)
+
+buton = Label(panel_2, text="Boton Panel")
+panel_2.add(buton)
+
+root.mainloop()
