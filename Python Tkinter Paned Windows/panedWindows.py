18dffe70db0ec2d5b7086a133ee26a78ed01ffad
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-21 22:01:48 -0500

    Python Tkinter Paned Windows start

---
+++ Python Tkinter Paned Windows/panedWindows.py
@@ -0,0 +1,14 @@
+# Python Tkinter Paned Windows
+# ventanas con paneles de Python Tkinter
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter Paned Windows!')
+root.iconbitmap('Python Tkinter Paned Windows/menu.ico')
+root.geometry("400x600") 
+
+
+
+root.mainloop()
