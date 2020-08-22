c2e86db5c291ca7d48c4026cacc0ed63f8b654a5
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-21 20:52:15 -0500

    Python Tkinter Menu Bars start

---
+++ Python Tkinter Menu Bars/menuBars.py
@@ -0,0 +1,15 @@
+# Python Tkinter Menu Bars
+# Barras de menú de Pyhton Tkinter
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter Menu Bars!')
+root.iconbitmap('Python Tkinter Menu Bars/menu.ico')
+root.geometry("400x600") 
+
+
+
+
+root.mainloop()
