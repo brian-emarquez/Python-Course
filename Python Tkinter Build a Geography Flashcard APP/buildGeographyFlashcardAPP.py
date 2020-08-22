2c33d1a2f3eaf568f305f0f8fc6d6df5b66a035d
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-22 16:18:27 -0500

    Python Tkinter Build a Geography Flashcard APP

---
+++ Python Tkinter Build a Geography Flashcard APP/buildGeographyFlashcardAPP.py
@@ -0,0 +1,11 @@
+# Python Tkinter Build a Geography Flashcard APP
+# Python Tkinter crea una aplicación de tarjeta de geografía
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Build a Geography Flashcard APP!')
+root.iconbitmap('Python Tkinter Build a Geography Flashcard APP/avatar.ico')
+root.geometry("400x400")
+
+root.mainloop()
