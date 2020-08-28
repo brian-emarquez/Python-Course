8d38a6336d1e4b2d7e2a39c5ca0c84e083cc3d88
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-28 13:54:09 -0500

    Python Tkinter Using.config() to update Widgets update

--- Pyhton Tkinter Using Config() to update/usingConfigtoUpdate.py
+++ Pyhton Tkinter Using Config() to update/usingConfigtoUpdate.py
@@ -9,7 +9,17 @@ root.iconbitmap('Pyhton Tkinter Using Config() to update/avatar.ico')
 root.geometry("400x600")
 
 
+def something():
+    my_label.config(text="This is new Text", font=("Helvetica", 8))
+    root.config(bg="blue")
+    my_button.config(text="You've been configged", state=DISABLED, pady=30)
 
+global my_label
+my_label = Label (root, text="This is my Text", font=("helvetica", 17))
+my_label.pack(pady=10)
 
+global my_button
+my_button = Button(root, text="Click Me", command=something)
+my_button.pack(pady=10)
 
 root.mainloop()
