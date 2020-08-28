c9d7eab3d7b1edb51ad2a64b9375995cd4f03c14
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-28 13:44:10 -0500

    Python Tkinter Using.config() to update Widgets start

---
+++ Pyhton Tkinter Using Config() to update/usingConfigtoUpdate.py
@@ -0,0 +1,15 @@
+# Python Tkinter Using.config() to update Widgets
+# 
+
+from tkinter import *
+
+root = Tk()
+root.title('Pyhton Tkinter Using Config() to update')
+root.iconbitmap('Pyhton Tkinter Using Config() to update/avatar.ico')
+root.geometry("400x600")
+
+
+
+
+
+root.mainloop()
