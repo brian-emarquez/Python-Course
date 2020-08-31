bdae082fc660223beff8e519353f452126ced9a1
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-31 14:41:03 -0500

    Python Tkinter How Validate an Entry Widget Integer start

---
+++ Python Tkinter How Validate an Entry Widget Integer/howValidadeEntryWidgetInteger.py
@@ -0,0 +1,16 @@
+# Python Tkinter How Validate an Entry Widget Integer
+# Python Tkinter Cómo validar un entero de widget de entrada
+
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How Validate an Entry Widget Integer')
+root.iconbitmap('Python Tkinter How Validate an Entry Widget Integer/api.ico')
+root.geometry("400x400")
+
+
+
+root.mainloop()
+
+
