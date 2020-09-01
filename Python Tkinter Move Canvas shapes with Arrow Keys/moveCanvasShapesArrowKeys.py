a2cd5af721c52207e229da9d1db4fb540aa4af8a
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-01 14:31:15 -0500

    Python Tkinter Move Canvas shapes with Arrow Keys start

---
+++ Python Tkinter Move Canvas shapes with Arrow Keys/moveCanvasShapesArrowKeys.py
@@ -0,0 +1,14 @@
+# Python Tkinter Move Canvas shapes with Arrow Keys
+# Python Tkinter Mover formas de lienzo con teclas de flecha
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Move Canvas shapes with Arrow Keys')
+root.iconbitmap('Python Tkinter Move Canvas shapes with Arrow Keys/api.ico')
+root.geometry("400x400")
+
+
+root.mainloop()
+
+
