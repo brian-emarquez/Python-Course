73f33fecef9ccf3e4d89ee89df5a37c69a8e3a06
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-01 12:51:07 -0500

    Python Tkinter How Draw Lines and Shapes with Canvas start

---
+++ Python Tkinter How Draw Lines and Shapes with Canvas/howDrawLinesShapesCanvas.py
@@ -0,0 +1,15 @@
+# Python Tkinter How Draw Lines and Shapes with Canvas
+# Python Tkinter How Draw Lines and Shapes with Canvas
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How Draw Lines and Shapes with Canvas')
+root.iconbitmap('Python Tkinter How Draw Lines and Shapes with Canvas/api.ico')
+root.geometry("400x400")
+
+
+
+root.mainloop()
+
+
