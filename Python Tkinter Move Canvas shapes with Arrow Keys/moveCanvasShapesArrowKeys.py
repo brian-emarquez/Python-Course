0f5a4eb02c12f226d7f8bf224d9650b4a965af8a
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-01 15:32:11 -0500

    Python Tkinter Move Canvas shapes with Arrow Keys update

--- Python Tkinter Move Canvas shapes with Arrow Keys/moveCanvasShapesArrowKeys.py
+++ Python Tkinter Move Canvas shapes with Arrow Keys/moveCanvasShapesArrowKeys.py
@@ -8,6 +8,51 @@ root.title('Python Tkinter Move Canvas shapes with Arrow Keys')
 root.iconbitmap('Python Tkinter Move Canvas shapes with Arrow Keys/api.ico')
 root.geometry("400x400")
 
+w = 600
+h = 400
+x = w//2
+y = h//2
+
+my_canvas = Canvas(root, width=w, height=h, bg="white")
+my_canvas.pack(pady=10)
+
+my_circle = my_canvas.create_oval(x, y, x+10, y+10)
+
+def left(event):
+    x = -10
+    y= 0
+    my_canvas.move(my_circle, x, y)
+
+def right(event):
+    x = 10
+    y= 0
+    my_canvas.move(my_circle, x, y)
+
+def up(event):
+    x =0
+    y= -10
+    my_canvas.move(my_circle, x, y)
+
+def down(event):
+    x =0
+    y= 10
+    my_canvas.move(my_circle, x, y)
+
+def pressing(event):
+    x = 0
+    y = 0
+    if event.char == "a" : x = -10
+    if event.char == "w" : x  = 10
+    if event.char == "s" : y = -10
+    if event.char == "d" : y =  10
+    my_canvas.move(my_circle, x, y)
+
+root.bind("<Key>", pressing)
+
+root.bind("<Left>", left)
+root.bind("<Right>", right)
+root.bind("<Up>", up)
+root.bind("<Down>", down)
 
 root.mainloop()
 
