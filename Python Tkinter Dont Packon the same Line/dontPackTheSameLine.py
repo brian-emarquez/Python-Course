9f5b132d31b7e489f4b56af606d5dee0650dfa68
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-09 18:19:42 -0500

    Python Tkinter Dont Packon the same Line start

---
+++ Python Tkinter Dont Packon the same Line/dontPackTheSameLine.py
@@ -0,0 +1,16 @@
+# Python Tkinter Dont Packon the same Line
+# Python Tkinter no empaqueta en la misma línea
+
+
+from tkinter import *
+from tkcalendar import *
+
+root = Tk()
+root.title('Python Tkinter Dont Packon the same Line')
+root.iconbitmap('Python Tkinter Dont Packon the same Line/api.ico')
+root.geometry("600x600")
+
+
+
+root.mainloop()
+
