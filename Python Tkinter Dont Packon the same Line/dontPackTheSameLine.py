a25d82ca95d56015c269c5bb97976adaee90b4a0
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-09 18:31:17 -0500

    Python Tkinter Dont Packon the same Line update

--- Python Tkinter Dont Packon the same Line/dontPackTheSameLine.py
+++ Python Tkinter Dont Packon the same Line/dontPackTheSameLine.py
@@ -10,6 +10,17 @@ root.title('Python Tkinter Dont Packon the same Line')
 root.iconbitmap('Python Tkinter Dont Packon the same Line/api.ico')
 root.geometry("600x600")
 
+def grab():
+    my_label.config(text=my_box.get())
+
+
+my_box = Entry(root, font=("Helvetica", 28))
+my_box.pack(pady=20)
+
+my_button = Button(root, text="Grab Text From Box", command=grab).pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
 
 
 root.mainloop()
