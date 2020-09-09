5b117296ce9f173c417fc3f41af8cd9faea9e544
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-09 18:03:17 -0500

    Python Tkinter Create a Data Picker Calendar start

---
+++ Python Tkinter Create a Data Picker Calendar/createDataPickerCalendar.py
@@ -0,0 +1,15 @@
+# Python Tkinter Create a Data Picker Calendar
+# Python Tkinter Crear un calendario de selector de datos
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Create a Data Picker Calendar')
+root.iconbitmap('Python Tkinter Create a Data Picker Calendar/api.ico')
+root.geometry("600x600")
+
+
+
+
+root.mainloop()
+
