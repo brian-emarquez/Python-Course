48818a092c4071a035a173f33b0c85f19f2540a4
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-09 18:36:35 -0500

    Python Tkinter How to Open External Programns start

---
+++ Python Tkinter How to Open External Programns/howOpenExternalPrograms.py
@@ -0,0 +1,12 @@
+# Python Tkinter How to Open External Programns
+# Python Tkinter Cómo abrir programas externos
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How to Open External Programns')
+root.iconbitmap('Python Tkinter How to Open External Programns/pineapple.ico')
+root.geometry("600x600") 
+
+
+root.mainloop()
