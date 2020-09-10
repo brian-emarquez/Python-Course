9d291431c0b8afe1516e97a7375b3ac329e870bc
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-10 09:56:00 -0500

    Python Tkinter Mouse On Hover Image Animation start

---
+++ Python Tkinter Mouse On Hover Image Animation/MouseOnHoverImageAnimation.py
@@ -0,0 +1,13 @@
+# Python Tkinter Mouse On Hover Image Animation
+# Animación de imagen con el mouse sobre el mouse
+
+from tkinter import *
+from tkinter import filedialog
+import os
+
+root = Tk()
+root.title('Python Tkinter Mouse On Hover Image Animation')
+root.iconbitmap('Python Tkinter Mouse On Hover Image Animation/pineapple.ico')
+root.geometry("600x600") 
+
+root.mainloop()
