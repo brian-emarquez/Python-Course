d03154f870dda85f0c2bab4208e06655c9945077
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-10 10:40:23 -0500

    Python Tkinter Text to Speech

---
+++ Python Tkinter Text to Speech/testToSpeech.py
@@ -0,0 +1,12 @@
+# Python Tkinter Text to Speech
+# Texto a voz de Python Tkinter
+
+from tkinter import *
+
+root = Tk()
+root.iconbitmap('Python Tkinter Text to Speech/pineapple.ico')
+root.geometry("600x600") 
+root.title('Python Tkinter Text to Speechn')
+
+
+root.mainloop()
