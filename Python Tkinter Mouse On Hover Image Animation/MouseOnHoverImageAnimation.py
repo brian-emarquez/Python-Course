5aaa848344a3134d715c90dc0b4592ea8d7850e0
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-10 10:36:26 -0500

    Python Tkinter Mouse On Hover Image Animation

--- Python Tkinter Mouse On Hover Image Animation/MouseOnHoverImageAnimation.py
+++ Python Tkinter Mouse On Hover Image Animation/MouseOnHoverImageAnimation.py
@@ -2,12 +2,32 @@
 # Animación de imagen con el mouse sobre el mouse
 
 from tkinter import *
-from tkinter import filedialog
-import os
 
 root = Tk()
-root.title('Python Tkinter Mouse On Hover Image Animation')
 root.iconbitmap('Python Tkinter Mouse On Hover Image Animation/pineapple.ico')
 root.geometry("600x600") 
+root.title('Python Tkinter Mouse On Hover Image Animation')
+
+def change(e):
+    my_pic = PhotoImage(file="Python Tkinter Mouse On Hover Image Animation/images/dreamtheater2.png")
+    my_label.config(image=my_pic)
+    my_label.image = my_pic
+
+def change_back(e):
+    my_pic = PhotoImage(file="Python Tkinter Mouse On Hover Image Animation/images/dreamtheater1.png")
+    my_label.config(image=my_pic)
+    my_label.image = my_pic
+
+def do_sumething():
+    label2 = Label(root, text="You clicked the button")
+    label2.pack()
+
+
+my_pic = PhotoImage(file="Python Tkinter Mouse On Hover Image Animation/images/dreamtheater1.png")
+my_label = Button(root, image=my_pic, command=do_sumething)
+my_label.pack(pady=20)
+
+my_label.bind("<Enter>", change)
+my_label.bind("<Leave>", change_back)
 
 root.mainloop()
