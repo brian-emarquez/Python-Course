190577fff30d087f6070eed6c7b1ff9cf68dbad8
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-10 12:44:33 -0500

    Python Tkinter How To Riseze Entry Box height start

---
+++ Python Tkinter How to Resize Images/howRisizeImages.py
@@ -0,0 +1,14 @@
+# Python Tkinter How To Riseze Entry Box height
+# Python Tkinter Cómo elevar la altura del cuadro de entrada
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How to Resize Images')
+root.iconbitmap('Python Tkinter How to Resize Images/pineapple.ico')
+root.geometry("800x500") 
+
+
+
+
+root.mainloop()
