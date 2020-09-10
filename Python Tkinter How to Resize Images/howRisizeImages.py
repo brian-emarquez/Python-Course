8aa0267cfc00b5f4421f3ff9127bde86b342ed46
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-10 13:49:28 -0500

    Python Tkinter How To Riseze Entry Box height update

--- Python Tkinter How to Resize Images/howRisizeImages.py
+++ Python Tkinter How to Resize Images/howRisizeImages.py
@@ -2,13 +2,23 @@
 # Python Tkinter Cómo elevar la altura del cuadro de entrada
 
 from tkinter import *
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title('Python Tkinter How to Resize Images')
 root.iconbitmap('Python Tkinter How to Resize Images/pineapple.ico')
 root.geometry("800x500") 
 
+# Open Image
+my_pic = Image.open("Python Tkinter How to Resize Images/image/moonglow.png")
 
+# Resize Image
+resized = my_pic.resize((250, 225), Image.ANTIALIAS)
 
+new_pic = ImageTk.PhotoImage(resized)
+
+# Image size 500x750 /2500
+my_label = Label(root, image=new_pic)
+my_label.pack(pady=20)
 
 root.mainloop()
