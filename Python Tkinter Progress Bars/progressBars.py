125baaf879fb1dbf1144c0927b05732db660f514
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 10:17:34 -0500

    Python Tkinter Progress Bars start

---
+++ Python Tkinter Progress Bars/progressBars.py
@@ -0,0 +1,16 @@
+# Python Tkinter Progress Bars
+# Barras de progreso de Python Tkinter
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Python Tkinter Progress Bars')
+root.iconbitmap('Python Tkinter Progress Bars/pineapple.ico')
+root.geometry("800x500") 
+
+
+
+
+
+root.mainloop()
