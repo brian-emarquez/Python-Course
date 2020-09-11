99b4785b18cdeda0632c4f4f5e4658c5e8fa5e64
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 11:15:47 -0500

    Python Tkinter How to Resize a Window Dynamically start

---
+++ Python Tkinter How to Resize a Window Dynamically/resizeWindowDynamically.py
@@ -0,0 +1,14 @@
+# Python Tkinter How to Resize a Window Dynamically
+# Python Tkinter Cómo cambiar el tamaño de una ventana dinámicamente
+
+from tkinter import *
+import time
+
+root = Tk()
+root.title('Python Tkinter How to Resize a Window Dynamically')
+root.iconbitmap('Python Tkinter How to Resize a Window Dynamically/pineapple.ico')
+root.geometry("600x400") 
+
+
+
+root.mainloop()
