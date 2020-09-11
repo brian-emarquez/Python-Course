da63a40523fac3bd8c3b898239301a4103f09d78
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 11:36:03 -0500

    Python Tkinter Build a Paint APP start

---
+++ Python Tkinter Build a Paint APP/buildPaintApp.py
@@ -0,0 +1,13 @@
+# Python Tkinter Build a Paint APP
+# Python Tkinter crea una aplicación de pintura
+
+from tkinter import *
+import time
+
+root = Tk()
+root.title('Python Tkinter Build a Paint APP')
+root.iconbitmap('Python Tkinter Build a Paint APP/paint.ico')
+root.geometry("800x800") 
+
+
+root.mainloop()
