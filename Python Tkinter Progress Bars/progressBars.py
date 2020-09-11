6ebc2e4f451a55781b5a4a4a31710ca8cdd806bc
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 10:39:09 -0500

    Python Tkinter Progress Bars update

--- Python Tkinter Progress Bars/progressBars.py
+++ Python Tkinter Progress Bars/progressBars.py
@@ -2,15 +2,35 @@
 # Barras de progreso de Python Tkinter
 
 from tkinter import *
-from PIL import ImageTk, Image
+from tkinter import ttk
+import time
 
 root = Tk()
 root.title('Python Tkinter Progress Bars')
 root.iconbitmap('Python Tkinter Progress Bars/pineapple.ico')
 root.geometry("800x500") 
 
+def step():
+    #my_progress.start(10)
+    for x in range(5):
+        my_label.config(text=my_progress['value'])
+        my_progress["value"] += 20
+        root.update_idletasks()
+        time.sleep(1)
+        
+def stop():
+    my_progress.stop()
 
+my_progress = ttk.Progressbar(root, orient=HORIZONTAL, length=300, mode="determinate")
+my_progress.pack(pady=20)
 
+my_button = Button(root, text="Progress", command=step)
+my_button.pack(pady=20)
 
+my_button2 = Button(root, text="stop", command=stop)
+my_button2.pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
 
 root.mainloop()
