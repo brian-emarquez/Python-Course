336b261dd87d143d29ec9ef44d51f54d00da437a
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 10:41:57 -0500

    Python Tkinter Timers and Clocks start

---
+++ Python Tkinter Timers and Clocks/timersClocks.py
@@ -0,0 +1,14 @@
+# Python Tkinter Timers and Clocks
+# Temporizadores y relojes de Python Tkinter
+
+from tkinter import *
+from tkinter import ttk
+import time
+
+root = Tk()
+root.title('Python Tkinter Timers and Clocks')
+root.iconbitmap('Python Tkinter Timers and Clocks/pineapple.ico')
+root.geometry("800x500") 
+
+
+root.mainloop()
