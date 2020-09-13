6ef03e2a7b851ce7049a9525b7d1a5f3a2303382
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-12 20:55:08 -0500

    Python Tkinter Build a Paint APP update 3

--- Python Tkinter Build a Paint APP/buildPaintApp.py
+++ Python Tkinter Build a Paint APP/buildPaintApp.py
@@ -12,6 +12,11 @@ class Paint():
         self.root.configure(background="white")
         self.root.resizable(0,0)
 
+        # imp thing
+        self.pen_color = 'black'
+
+
+
         # adding widgeths to tkinter window
         self.color_frame = LabelFrame(self.root, text="color", font = ('arial, 13'), bd=5, relief=RIDGE, bg="white")
         self.color_frame.place(x=0, y=0, width=70, height=185)
@@ -19,7 +24,7 @@ class Paint():
         colors = ['#ff0000', '#ff4dd2', '#ffff33', '#000000', '#0066ff','#660033','#4dff4d', '#b300b3', '#00ffff', '#00ffff', '#808080', '#99ffcc']
         i = j =0
         for color in colors:
-            Button(self.color_frame, bg=color, bd=2, relief=RIDGE, width=3, command=None).grid(row=i, column=j)
+            Button(self.color_frame, bg=color, bd=2, relief=RIDGE, width=3, command=lambda col =color:self.select_color(col)).grid(row=i, column=j)
             i +=1
             if i==6:
                 i=0
@@ -38,7 +43,6 @@ class Paint():
         self.canvas_color_button.place(x=0, y=277)
 
         # creating a scale for pen and araser size..
-
         self.pen_size_scale_frame = LabelFrame(self.root, text="size", bd=5, bg="white", font=('arial', 15, 'bold'), relief=RIDGE)
         self.pen_size_scale_frame.place(x=0, y=310, height=200, width=70)
 
@@ -49,6 +53,20 @@ class Paint():
         # creating canvas
         self.canvas = Canvas (self.root, bg='white', bd=5, relief=GROOVE, height=500, width=700)
         self.canvas.place(x=80, y=0)
+
+        # bind the canvas with mouse drag
+        self.canvas.bind("<B1-Motion>", self.paint)
+
+    # funtions are defined here
+    def paint(self, event):
+        #print("mouse es dragged")
+        x1, y1 = (event.x-2), (event.y-2)
+        x2, y2 = (event.x + 2), (event.y + 2)
+
+        self.canvas.create_oval(x1, y1, x2, y2, fill=self.pen_color, outline=self.pen_color, width=self.pen_size.get())
+
+    def select_color(self, col):
+        self.pen_color = col
 
 
 if __name__=="__main__":
