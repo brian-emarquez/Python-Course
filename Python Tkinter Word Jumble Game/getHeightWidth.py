7b20f27d758474511393ba10ee31fee412e9a778
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-14 16:56:22 -0500

    Pyhton Tkinter Get Height and Width update 1

--- Python Tkinter Word Jumble Game/getHeightWidth.py
+++ Python Tkinter Word Jumble Game/getHeightWidth.py
@@ -6,9 +6,13 @@ from tkinter import *
 root = Tk()
 root.title('Pyhton Tkinter Get Height and Width')
 root.iconbitmap('Pyhton Tkinter Get Height and Width/liam.ico')
-root.geometry("800x800+-200+100") 
+root.geometry("600x400+-200+100") 
 
 
+def suffle():
+    my_label = Label(root, text="", font("Helvetica", 48))
+    my_label.pack()
 
+states = ['ancash', 'apurimac', 'arequipa', 'ayacucho', 'cajamarca', 'cerro de pasco', 'cuzco', 'huancavelica', 'huanuco', 'ica', 'junin', 'la libertad', 'lambayeque', 'lima', 'madre de dios', 'moquegua', 'piura', 'puno', 'san martin', 'tacna', 'tumbes', 'ucayali', 'amazonas', 'loreto']
 
 root.mainloop()
