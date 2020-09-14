16aa255c0ea1c5c81f3141c5194b4e805b062e51
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-14 15:17:39 -0500

    Pyhton Tkinter Get Height and Width start

---
+++ Python Tkinter Word Jumble Game/getHeightWidth.py
@@ -0,0 +1,14 @@
+# Pyhton Tkinter Get Height and Width
+# Pyhton Tkinter Obtener altura y ancho
+
+from tkinter import *
+
+root = Tk()
+root.title('Pyhton Tkinter Get Height and Width')
+root.iconbitmap('Pyhton Tkinter Get Height and Width/liam.ico')
+root.geometry("800x800+-200+100") 
+
+
+
+
+root.mainloop()
