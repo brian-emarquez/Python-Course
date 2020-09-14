b2735927633838ef5301c9f820c21f66221c584a
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-14 14:51:14 -0500

    Pyhton Tkinter Get Height and Width

---
+++ Pyhton Tkinter Get Height and Width/getHeightWidth.py
@@ -0,0 +1,17 @@
+# Pyhton Tkinter Get Height and Width
+# Pyhton Tkinter Obtener altura y ancho
+
+from tkinter import *
+
+root = Tk()
+root.title('Pyhton Tkinter Get Height and Width')
+root.iconbitmap('Pyhton Tkinter Get Height and Width/liam.ico')
+root.geometry("800x800+-1900+100") 
+
+
+
+
+
+
+
+root.mainloop()
