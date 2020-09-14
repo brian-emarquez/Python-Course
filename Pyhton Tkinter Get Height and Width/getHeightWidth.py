85a7a36cb85488dc27e5aa0f3341907237401d90
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-14 15:14:12 -0500

    Pyhton Tkinter Get Height and Width complete

--- Pyhton Tkinter Get Height and Width/getHeightWidth.py
+++ Pyhton Tkinter Get Height and Width/getHeightWidth.py
@@ -6,10 +6,24 @@ from tkinter import *
 root = Tk()
 root.title('Pyhton Tkinter Get Height and Width')
 root.iconbitmap('Pyhton Tkinter Get Height and Width/liam.ico')
-root.geometry("800x800+-1900+100") 
+root.geometry("800x800+-200+100") 
 
+def info():
+    dimension_label = Label(root, text=root.winfo_geometry())
+    dimension_label.pack(pady=20)
 
+    height_label = Label(root, text="Height: " + str(root.winfo_height()))
+    height_label.pack(pady=20)
+    width_label = Label(root, text="Width: " + str(root.winfo_width()))
+    width_label.pack(pady=10)
 
+    x_label = Label(root, text="X: " + str(root.winfo_x()))
+    x_label.pack(pady=20)
+    y_label = Label(root, text="Y " + str(root.winfo_y()))
+    y_label.pack(pady=10)
+
+my_button = Button(root, text="ClickMe", command=info)
+my_button.pack(pady=20)
 
 
 
