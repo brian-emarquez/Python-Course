23716b8e6555dabbdb1c258de4a83180e44ac796
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-15 11:36:57 -0500

    Python Tkinter Sounds and Music start

---
+++ Python Tkinter Sounds and Music/soundsMusic.py
@@ -0,0 +1,19 @@
+# Python Tkinter Sounds and Music
+# Sonidos y música de Python Tkinter
+
+from tkinter import *
+from random import choice
+from random import shuffle
+
+root = Tk()
+root.title('Python Tkinter Sounds and Music')
+root.iconbitmap('Python Tkinter Sounds and Music/sound.ico')
+root.geometry("500x400") 
+
+
+
+
+
+
+
+root.mainloop()
