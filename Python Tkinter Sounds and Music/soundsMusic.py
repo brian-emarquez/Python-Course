6ea93e6c193fd74e9fb5a00ca0f99a4d849d34d0
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-15 12:35:38 -0500

    Python Tkinter Sounds and Music complete

--- Python Tkinter Sounds and Music/soundsMusic.py
+++ Python Tkinter Sounds and Music/soundsMusic.py
@@ -5,18 +5,27 @@
 
 
 from tkinter import *
-from random import choice
-from random import shuffle
-
+import pygame
 root = Tk()
 root.title('Python Tkinter Sounds and Music')
 root.iconbitmap('Python Tkinter Sounds and Music/sound.ico')
 root.geometry("500x400") 
 
+pygame.mixer.init()
 
+def play():
+    pygame.mixer.music.load("Python Tkinter Sounds and Music/sound/rock-808.mp3")
+    pygame.mixer.music.play(loops=0)
 
+def stop():
+    pygame.mixer.music.stop()
+    
 
+my_button = Button(root, text="Play Song", font=("Helvetica", 32), command=play)
+my_button.pack(pady=20)
 
+stop_button = Button(root, text="Stop", command=stop)
+stop_button.pack(pady=20)
 
 
 root.mainloop()
