e00c017d16d0947451a0b489ca7de58f87fe1a7a
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-14 19:38:50 -0500

    Python Tktinter Button Mouse on-Hover Popup Message start

---
+++ Python Tktinter Button Mouse on-Hover Popup Message/buttonMousePupupMessage.py
@@ -0,0 +1,16 @@
+# Python Tktinter Button Mouse on-Hover Popup Message
+# PMensaje emergente de desplazamiento del mouse del botón Tktinter de Python
+
+from tkinter import *
+from random import choice
+from random import shuffle
+
+root = Tk()
+root.title('Python Tktinter Button Mouse on-Hover Popup Message')
+root.iconbitmap('Python Tktinter Button Mouse on-Hover Popup Message/elvis.ico')
+root.geometry("600x400+-50+50") 
+
+
+
+
+root.mainloop()
