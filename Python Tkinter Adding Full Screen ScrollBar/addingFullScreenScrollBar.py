d6adbcbb904fa3735b99ee1de56ab0b1959948e2
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-22 13:57:12 -0500

    Python Tkinter Adding Full Screen ScrollBar start

---
+++ Python Tkinter Adding Full Screen ScrollBar/addingFullScreenScrollBar.py
@@ -0,0 +1,12 @@
+# Python Tkinter Adding Full Screen ScrollBar
+from tkinter import *
+
+root = Tk()
+root.title('Learn To Code at Python Tkinter Adding Full Screen ScrollBar')
+root.iconbitmap('Python Tkinter Adding Full Screen ScrollBar/panda.ico')
+root.geometry("500x400")
+
+
+
+
+root.mainloop()
