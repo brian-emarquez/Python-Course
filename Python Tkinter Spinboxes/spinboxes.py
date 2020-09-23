039458c6daadf75997331b2a6d3a11097e09b6f3
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-23 17:35:14 -0500

    Python Tkinter spinboxes complete

--- Python Tkinter Spinboxes/spinboxes.py
+++ Python Tkinter Spinboxes/spinboxes.py
@@ -8,5 +8,18 @@ root.title('Learn To Code at Python Python Tkinter Spinboxes')
 root.iconbitmap('Python Tkinter Spinboxes/panda.ico')
 root.geometry("500x400")
 
+def grab():
+    my_label.configure(text=my_spin.get())
+
+names = ("Brian", "Maria", "Jack")
+#my_spin = Spinbox(root, from_=0, to=10, increment = 2, font=("Helvetica", 20))
+my_spin = Spinbox(root, from_=0, values=names, to=10, font=("Helvetica", 20))
+my_spin.pack(pady=20)
+
+my_button = Button(root, text="Submit", command=grab)
+my_button.pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
 
 root.mainloop()
