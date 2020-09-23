e745c8703270dcbb25acc065774cc983a3b6395b
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-23 18:02:12 -0500

    Python Tkinter Read and Write to Text Files start

---
+++ Python Tkinter Read and Write to Text Files/readWriteTextFiles.py
@@ -0,0 +1,14 @@
+# Python Tkinter Read and Write to Text Files
+# Python Tkinter leer y escribir en archivos de texto
+
+from tkinter import *
+
+root = Tk()
+
+root.title('Learn To Code at Python Tkinter Text Box')
+root.iconbitmap('Python Tkinter Text-Box/panda.ico')
+root.geometry("600x600")
+
+
+
+root.mainloop()
