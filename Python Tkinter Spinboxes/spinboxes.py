ce2588db7f9913688c9f03e650cd728420f2fba4
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-23 17:16:49 -0500

    Python Tkinter spinboxes start

---
+++ Python Tkinter Spinboxes/spinboxes.py
@@ -0,0 +1,12 @@
+# Python Tkinter spinboxes
+
+from tkinter import *
+
+root = Tk()
+
+root.title('Learn To Code at Python Python Tkinter Spinboxes')
+root.iconbitmap('Python Tkinter Spinboxes/panda.ico')
+root.geometry("500x400")
+
+
+root.mainloop()
--- Python Tkinter Threading/thereading.py
+++ Python Tkinter Threading/thereading.py
@@ -7,8 +7,8 @@ import threading
 
 root = Tk()
 
-root.title('Learn To Code at Python Pythin Tkinter Threading')
-root.iconbitmap('Pythin Tkinter Threading/panda.ico')
+root.title('Learn To Code at Python Python Tkinter Threading')
+root.iconbitmap('Python Tkinter Threading/panda.ico')
 root.geometry("500x400")
 
 def five_seconds():
