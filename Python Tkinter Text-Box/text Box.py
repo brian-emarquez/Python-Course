a63f8845a5ab669d4514b1df80de8c381f7a3e70
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-23 17:38:02 -0500

    Python Tkinter Text Box start

---
+++ Python Tkinter Text-Box/text Box.py
@@ -0,0 +1,14 @@
+# Python Tkinter Text Box
+# Caja de texto
+
+from tkinter import *
+
+root = Tk()
+
+root.title('Learn To Code at Python Tkinter Text Box')
+root.iconbitmap('Python Tkinter Text-Box/panda.ico')
+root.geometry("500x400")
+
+
+
+root.mainloop()
