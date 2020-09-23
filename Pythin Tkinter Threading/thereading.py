5ad1af29275f16860273f07c4fc78ab46885e9d2
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-23 16:38:28 -0500

    Python Tkinter Threading start

---
+++ Pythin Tkinter Threading/thereading.py
@@ -0,0 +1,15 @@
+# Python Tkinter Threading
+
+from tkinter import *
+
+root = Tk()
+root.title('Learn To Code at Python Pythin Tkinter Threading')
+root.iconbitmap('Pythin Tkinter Threading/panda.ico')
+root.geometry("500x400")
+
+
+
+
+
+
+root.mainloop()
