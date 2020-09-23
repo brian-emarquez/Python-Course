de30edbcb7c8d1cead5ffddb489860217037976d
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-23 17:11:17 -0500

    Python Tkinter Threading complete

--- Pythin Tkinter Threading/thereading.py
+++ Pythin Tkinter Threading/thereading.py
@@ -22,7 +22,7 @@ def rando():
 my_label = Label(root, text="Hello There!")
 my_label.pack(pady=20)
 
-my_button1 = Button(root, text="5 seconds", command=threading)
+my_button1 = Button(root, text="5 seconds", command=threading.Thread(target=five_seconds).start())
 my_button1.pack(pady=20)
 
 my_button2 =  Button(root, text="Pick Random Number", command=rando)
@@ -30,5 +30,7 @@ my_button2.pack(pady=20)
 
 random_label = Label(root, text="")
 random_label.pack(pady=20)
+
+threading.Thread(target=five_seconds).start() # mostr el mensaje automaticamente
 
 root.mainloop()
