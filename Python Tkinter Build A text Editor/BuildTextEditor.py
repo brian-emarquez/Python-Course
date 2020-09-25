4eb6788f801e8af4472c74beba61ee77e4fd8561
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-24 20:26:14 -0500

    Python Tkinter Build A text Editor update

---
+++ Python Tkinter Build A text Editor/BuildTextEditor.py
@@ -0,0 +1,15 @@
+# Python Tkinter Build A text Editor
+# Python Tkinter Build A editor de texto
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
+root = Tk()
+root.title('Python Tkinter Build A text Editor')
+root.iconbitmap('Python Tkinter Build A text Editor/icons/document.ico')
+root.geometry("700x500")
+
+
+
+root.mainloop()
