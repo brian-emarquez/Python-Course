65aa154dd910ccb6dfb0b74cd3b4d967500b130c
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-30 15:17:39 -0500

    Python Tkinter Tic Tac Toe Game start

---
+++ Python Tkinter Tic Tac Toe Game/ticTacToe.py
@@ -0,0 +1,15 @@
+# Python Tkinter Tic Tac Toe Game
+
+from tkinter import *
+
+
+root = Tk()
+
+root.title('Python Tkinter Tic Tac Toe Game')
+root.iconbitmap('Python Tkinter Tic Tac Toe Game/Icons/tic-tac-toe.ico')
+root.geometry("500x400")
+
+
+
+
+root.mainloop()
