0bd167ced7c1be1993f3a6ece61b51809299d590
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-29 20:16:39 -0500

    Python Tkinter Build A text Editor V - Undo Redo and Horizontal Scrollbar update 2

--- Python Tkinter Build A text Editor V/BuildTextEditorV.py
+++ Python Tkinter Build A text Editor V/BuildTextEditorV.py
@@ -136,12 +136,17 @@ my_frame.pack(pady=5)
 text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
+# Horizontal Scrollbar
+hor_scroll = Scrollbar(my_frame, orient='horizontal')
+hor_scroll.pack(side=BOTTOM, fill=X)
+
 # Create Text Box
-my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="#4FDECD", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="#4FDECD", selectforeground="black", undo=True, yscrollcommand=text_scroll.set, wrap="none",  xscrollcommand=hor_scroll.set)
 my_text.pack()
 
 # Configure our Scroolbar
 text_scroll.config(command=my_text.yview)
+hor_scroll.config(command=my_text.xview)
 
 # Create Menu
 my_menu = Menu(root)
@@ -154,6 +159,7 @@ file_menu.add_command(label="New", command=new_file)
 file_menu.add_command(label="Open", command=open_file)
 file_menu.add_command(label="Save", command=save_file)
 file_menu.add_command(label="Save As", command=save_as_file)
+file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 
 # Add Edit Menu
@@ -162,8 +168,9 @@ my_menu.add_cascade(label="Edit", menu=edit_menu)
 edit_menu.add_command(label="Cut", command=lambda: cut_text(False), accelerator="(Ctrl+x)")
 edit_menu.add_command(label="Copy", command=lambda: copy_text(False), accelerator="(Ctrl+c)")
 edit_menu.add_command(label="Paste", command=lambda: paste_text(False), accelerator="(Ctrl+v)")
-edit_menu.add_command(label="Undo")
-edit_menu.add_command(label="Redo")
+edit_menu.add_separator()
+edit_menu.add_command(label="Undo", command=my_text.edit_undo, accelerator="(Ctrl+z)")
+edit_menu.add_command(label="Redo", command=my_text.edit_redo, accelerator="(Ctrl+y)")
 
 # Add Status Bar To Botton of App
 status_bar = Label(root, text="Ready        ", anchor=E)
