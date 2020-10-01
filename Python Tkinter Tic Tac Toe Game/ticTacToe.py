23b7d2846859069b1318e74668ba65a35a370033
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-01 12:33:09 -0500

    Python Tkinter Tic Tac Toe Game update 2

--- Python Tkinter Tic Tac Toe Game/ticTacToe.py
+++ Python Tkinter Tic Tac Toe Game/ticTacToe.py
@@ -10,22 +10,42 @@ root.title('Python Tkinter Tic Tac Toe Game')
 root.iconbitmap('Python Tkinter Tic Tac Toe Game/Icons/tic-tac-toe.ico')
 #root.geometry("500x400")
 
-def  b_cick(b):
-    pass
+# X starts so true
+clicked = True
+count = 0 
+
+# Button clicked function
+def  b_click(b):
+    global clicked, count
+
+    if b["text"] == " " and clicked == True:
+        b["text"] = "X"
+        clicked = False
+        count += 1
+
+    elif b["text"] == " " and clicked == False:
+        b["text"] = "O"
+        clicked = True
+        count += 1
+    else:
+        messagebox.showerror("Tic Tac Toe", "Hey ! the box has ready been selected○\nPick Another Box...")
+
+
+
 
 
 # Build our Buttons
-b1 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b1))
-b2 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b2))
-b3 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b3))
+b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
+b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
+b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
 
-b4 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b4))
-b5 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b5))
-b6 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b6))
+b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
+b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
+b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
 
-b7 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b7))
-b8 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b8))
-b9 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_cick(b9))
+b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
+b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
+b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
 
 # Grid our buttons to the screen
 b1.grid(row=0, column=0)
