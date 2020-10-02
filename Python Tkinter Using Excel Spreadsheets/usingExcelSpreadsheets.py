39e23f0774a3a91c9ab33d8f8bd0efed39eb394d
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-01 21:55:22 -0500

    Python Tkinter Using Excel Spreadsheets start

---
+++ Python Tkinter Using Excel Spreadsheets/usingExcelSpreadsheets.py
@@ -0,0 +1,18 @@
+#----------------------------------------------------------#
+# Python Tkinter Using Excel Spreadsheets
+# Python Tkinter usando hojas de cálculo de Excel
+
+#----------------------------------------------------------#
+
+from tkinter import *
+from tkinter import messagebox
+
+
+root = Tk()
+
+root.title('Python Tkinter Using Excel Spreadsheets')
+root.iconbitmap('Python Tkinter Using Excel Spreadsheets/Icons/file.ico')
+root.geometry("500x400")
+
+
+root.mainloop()
