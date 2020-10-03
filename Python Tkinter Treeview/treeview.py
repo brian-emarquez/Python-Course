c5e949d517179e71d1ce17bc05f8e9b3ffc55cff
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-03 11:06:41 -0500

    Python Tkinter Treeview start

---
+++ Python Tkinter Treeview/treeview.py
@@ -0,0 +1,16 @@
+# Python Tkinter Treeview
+# Vista de árbol de Python Tkinter
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Treeview')
+root.iconbitmap('Python Tkinter Treeview/Icons/cocoa.ico')
+root.geometry("400x400") 
+
+
+
+
+
+
+root.mainloop()
