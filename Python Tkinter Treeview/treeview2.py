88369ab7dc93d7d73d1eaab67b3cd8feb4f810a2
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-05 11:52:23 -0500

    Python Tkinter Treeview 2

--- Python Tkinter Treeview/treeview2.py
+++ Python Tkinter Treeview/treeview2.py
@@ -15,29 +15,29 @@ my_tree = ttk.Treeview(root)
 my_tree["columns"] = ("Name", "ID", "Favorite Pizza")
 
 # Formate Our Columns
-my_tree.column("#0", width=120, minwidth=25)
+my_tree.column("#0", width=0, stretch=NO)
 my_tree.column("Name", anchor=W, width=120)
 my_tree.column("ID", anchor=CENTER, width=80)
 my_tree.column("Favorite Pizza", anchor=CENTER, width=120)
  
 # Create Headings
-my_tree.heading("#0", text="Label", anchor=W)
+my_tree.heading("#0", text="", anchor=W)
 my_tree.heading("Name", text="Name", anchor=W)
 my_tree.heading("ID", text="ID", anchor=CENTER)
 my_tree.heading("Favorite Pizza", text="Favorite Pizza", anchor=W)
 
 
 # Add Data
-my_tree.insert(parent='', index='end', iid=0, text="Parent", values=("Jhon", 1, "Peperroni"))
-my_tree.insert(parent='', index='end', iid=1, text="Parent", values=("Mary", "2", "Cheese"))
-my_tree.insert(parent='', index='end', iid=2, text="Parent", values=("Tina", "3", "Ham"))
-my_tree.insert(parent='', index='end', iid=3, text="Parent", values=("Bob", "4", "Supreme"))
-my_tree.insert(parent='', index='end', iid=4, text="Parent", values=("Erin", "5", "Cheese"))
-my_tree.insert(parent='', index='end', iid=5, text="Parent", values=("Wes", "6", "Onion"))
+my_tree.insert(parent='', index='end', iid=0, text="", values=("Jhon", 1, "Peperroni"))
+my_tree.insert(parent='', index='end', iid=1, text="", values=("Mary", "2", "Cheese"))
+my_tree.insert(parent='', index='end', iid=2, text="", values=("Tina", "3", "Ham"))
+my_tree.insert(parent='', index='end', iid=3, text="", values=("Bob", "4", "Supreme"))
+my_tree.insert(parent='', index='end', iid=4, text="", values=("Erin", "5", "Cheese"))
+my_tree.insert(parent='', index='end', iid=5, text="", values=("Wes", "6", "Onion"))
 
 #add child
-my_tree.insert(parent='', index='end', iid=6, text="Child", values=("Steve", "1.2", "Peppers"))
-my_tree.move("6", "0", "0")
+#my_tree.insert(parent='', index='end', iid=6, text="Child", values=("Steve", "1.2", "Peppers"))
+#my_tree.move("6", "0", "0")
 # Pack to the screen
 my_tree.pack(pady=20)
 
