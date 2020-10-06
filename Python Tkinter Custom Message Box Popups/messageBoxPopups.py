9bfe897f8e97f1116509132829ea68a6d628bbc0
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-06 13:42:08 -0500

    # Python Tkinter Custom Message Box Popupps

---
+++ Python Tkinter Custom Message Box Popups/messageBoxPopups.py
@@ -0,0 +1,14 @@
+# Python Tkinter Custom Message Box Popups
+# Ventanas emergentes del cuadro de mensaje personalizado de Python Tkinter
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('Python Tkinter Custom Message Box Popups')
+root.iconbitmap('Python Tkinter Custom Message Box Popups/Icons/like.ico')
+root.geometry("700x700") 
+
+
+
+root.mainloop()
