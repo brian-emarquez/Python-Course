a32586362f93d2b479a65a16015d1c009439ab12
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 17:53:46 -0500

    feat Python Tkinter Using Other Python Programs in you App start
    Python Tkinter Using Other Python Programs in you App start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Using Other Python Programs in you App/usingOtherPythonProgram.py
@@ -0,0 +1,14 @@
+#  Python Tkinter Using Other Python Programs in you App
+#  Python Tkinter usando otros programas de Python en su aplicación
+
+from tkinter import *
+from datetime import date
+
+root = Tk()
+root.title("Python Tkinter Using Other Python Programs in you App")
+root.iconbitmap("Python Tkinter Using Other Python Programs in you App/icons/h.ico")
+root.geometry('500x350')
+
+
+
+root.mainloop()
