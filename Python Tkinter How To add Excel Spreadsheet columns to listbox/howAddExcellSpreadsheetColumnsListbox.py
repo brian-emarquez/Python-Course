a290979cc81c6a625b89306acae2b5e46087d186
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 11:14:38 -0500

    feat: Python Tkinter How To add Excel Spreadsheet columns to listbox start
    Python Tkinter How To add Excel Spreadsheet columns to listbox start
    
    resolves:
    see also:

---
+++ Python Tkinter How To add Excel Spreadsheet columns to listbox/howAddExcellSpreadsheetColumnsListbox.py
@@ -0,0 +1,12 @@
+# Python Tkinter How To add Excel Spreadsheet columns to listbox
+# Python Tkinter Cómo agregar columnas de hoja de cálculo de Excel al cuadro de lista
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How To add Excel Spreadsheet columns to listbox')
+root.iconbitmap('Python Tkinter How To add Excel Spreadsheet columns to listbox/icons/excel.ico')
+root.geometry("500x500")
+
+
+root.mainloop()
