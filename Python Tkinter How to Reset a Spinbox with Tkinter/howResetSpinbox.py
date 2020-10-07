55348be0c2b21172b9fe6b0ac312adc4764904bb
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 18:51:21 -0500

    feat Python Tkinter How to Reset a Spinbox with Tkinter start
    Python Tkinter How to Reset a Spinbox with Tkinter start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter How to Reset a Spinbox with Tkinter/howResetSpinbox.py
@@ -0,0 +1,15 @@
+#  Python Tkinter How to Reset a Spinbox with Tkinter
+#  Python Tkinter Cómo restablecer un Spinbox con Tkinter
+
+from tkinter import *
+from namer import nameit
+
+root = Tk()
+root.title("Python Tkinter How to Reset a Spinbox with Tkinter")
+root.iconbitmap("Python Tkinter How to Reset a Spinbox with Tkinter/icons/h.ico")
+root.geometry('500x350')
+
+
+
+
+root.mainloop()
