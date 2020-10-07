a9687deffaff6508c01b26a5b1fffc9529867b4f
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 16:52:21 -0500

    feat Python tkinter How to Disable or Delete a Menu item complete
    Python tkinter How to Disable or Delete a Menu item complete
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Dates and 2020 Countdown App/dates2020CountdownApp.py
@@ -0,0 +1,15 @@
+#  Python Tkinter Dates and 2020 Countdown App
+#  Python Tkinter Dates y la aplicación Countdown 2020
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter Dates and 2020 Countdown App")
+root.iconbitmap("Python Tkinter Dates and 2020 Countdown App/icons/h.ico")
+root.geometry('500x500')
+
+
+
+
+
+root.mainloop()
