22a1b05283033defdbf5b73cd0a450f837050edf
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 14:26:09 -0500

    feat: Python Tkinter How to Disable or delete a menu item start
    Create carpet Python Tkinter How to disable or delete a menu item
    
    resolves:
    see also:

---
+++ Python tkinter How to Disable or Delete a Menu item/howToDisableDeleteMenuItem.py
@@ -0,0 +1,10 @@
+#  Python tkinter How to Disable or Delete a Menu item
+#  Python tkinter Cómo deshabilitar o eliminar un elemento del menú
+
+from tkinter import *
+
+root = Tk()
+root.title("Python tkinter How to Disable or Delete a Menu item")
+root.iconbitmap("Python tkinter How to Disable or Delete a Menu item/icons/cat_blacK.ico")
+
+root.mainloop()
