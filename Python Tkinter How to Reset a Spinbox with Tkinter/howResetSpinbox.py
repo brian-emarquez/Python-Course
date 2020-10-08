110cbe153f54efc334d5b06034c35e4d0b35d74f
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 19:17:31 -0500

    feat Python Tkinter How to Reset a Spinbox with Tkinter complete
    Python Tkinter How to Reset a Spinbox with Tkinter complete
    
    resolvesr: #
    se also: #

--- Python Tkinter How to Reset a Spinbox with Tkinter/howResetSpinbox.py
+++ Python Tkinter How to Reset a Spinbox with Tkinter/howResetSpinbox.py
@@ -2,14 +2,33 @@
 #  Python Tkinter Cómo restablecer un Spinbox con Tkinter
 
 from tkinter import *
-from namer import nameit
 
 root = Tk()
 root.title("Python Tkinter How to Reset a Spinbox with Tkinter")
 root.iconbitmap("Python Tkinter How to Reset a Spinbox with Tkinter/icons/h.ico")
 root.geometry('500x350')
 
+def reset():
+    var = IntVar(root)
+    var.set(0)
+    my_spin.config(textvariable=var)
 
+# set intvar
+var = IntVar(root)
+var.set(20)
 
+# set stringvar
+var2 = StringVar(root)
+var2.set("john")
+
+my_spin = Spinbox(root, 
+    from_=0, to=100,
+    font=("helvetica", 20),
+    textvariable=var)
+
+my_spin.pack(pady=20)
+
+my_button = Button(root, text="reset spinner", command=reset)
+my_button.pack(pady=20)
 
 root.mainloop()
--- Python Tkinter How to Reset a Spinbox with Tkinter/namer.py
+++
@@ -1,3 +0,0 @@
-def nameit(name):
-    greetings = f'Hello {name}'
-    return greetings
