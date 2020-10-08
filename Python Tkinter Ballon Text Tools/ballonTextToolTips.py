5e353f0a51b5687dbc8030c517da5fe2b237149d
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 19:27:04 -0500

    feat Python Tkinter Ballon Text Tools start
    Python Tkinter Ballon Text Tools start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Ballon Text Tools/ballonTextToolTips.py
@@ -0,0 +1,11 @@
+#  Python Tkinter Ballon Text Tools
+#  Herramientas de texto de globo de Python Tkinter
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter Ballon Text Tools")
+root.iconbitmap("Python Tkinter Ballon Text Tools/icons/balloons.ico")
+root.geometry('500x350')
+
+root.mainloop()
