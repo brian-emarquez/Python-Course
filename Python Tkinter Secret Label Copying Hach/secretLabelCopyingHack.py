172f2c639761e67bef318df2a9d83d840abf4289
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-10 17:08:19 -0500

    feat: Python Django Proyecto Web XV complete
    Python Tkinter Secret Label Copying Hach complete
    
    resolves:
    see also:

---
+++ Python Tkinter Secret Label Copying Hach/secretLabelCopyingHack.py
@@ -0,0 +1,33 @@
+# Python Tkinter Secret Label Copying Hach
+# Copia de etiqueta secreta de Python Tkinter Hach
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Secret Label Copying Hach')
+root.iconbitmap('Python Tkinter Secret Label Copying Hach/icons/secret.ico')
+root.geometry("500x350") 
+
+# Create Label
+my_label = Label(root, text="Text 1", font=("Helvetica", 20))
+my_label.pack(pady=20)
+
+# Create stringvar
+#my_text = StringVar()
+#my_text.set("This is Label 2")
+
+# Creaste Entry Box
+my_entry = Entry(root,
+    font=("Helvetica", 20), 
+    bd=0,
+    width=10)
+   # state="readonly")
+    #textvariable=my_text)
+
+my_entry.insert(0, "This is cool label 2")
+my_entry.config(state="readonly")
+
+my_entry.pack(pady=20)
+
+
+root.mainloop()
