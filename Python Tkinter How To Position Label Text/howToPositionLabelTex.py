c465288a5123c088bb35a1578686319d9a83d795
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-12 11:25:40 -0500

    feat: Python Tkinter How To Position Label Text start
    Python Tkinter How to position Label Text Start
    
    resolve:
    see also:

---
+++ Python Tkinter How To Position Label Text/howToPositionLabelTex.py
@@ -0,0 +1,16 @@
+# Python Tkinter How To Position Label Text
+# Python Tkinter Cómo colocar el texto de la etiqueta
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter How To Position Label Text')
+root.iconbitmap('Python Tkinter How To Position Label Text/icons/label.ico')
+root.geometry("600x600") 
+
+
+
+root.mainloop()
+
+
