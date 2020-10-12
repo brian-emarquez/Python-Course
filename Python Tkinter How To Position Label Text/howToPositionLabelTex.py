0db3f438f7f8bfbcdad7fe29874af4b04b0e814e
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-12 11:38:40 -0500

    feat: Python Tkinter How To Position Label Text Complete
    Python Tkinter How to position Label Text Complete
    
    resolve:
    see also:

--- Python Tkinter How To Position Label Text/howToPositionLabelTex.py
+++ Python Tkinter How To Position Label Text/howToPositionLabelTex.py
@@ -3,12 +3,30 @@
 
 from tkinter import *
 
-
 root = Tk()
 root.title('Python Tkinter How To Position Label Text')
 root.iconbitmap('Python Tkinter How To Position Label Text/icons/label.ico')
-root.geometry("600x600") 
+root.geometry("500x500") 
 
+my_label1 = Label(root,
+    text="Brian\nBrian Brian\nBrian Brian Brian",
+    font=("Comic Sans", 18),
+    bd=1, relief="sunken")
+my_label1.pack(pady=20, ipady=10, ipadx=10)
+
+my_label2 = Label(root,
+    text="Brian\nBrian Brian\nBrian Brian Brian",
+    font=("Comic Sans", 18),
+    bd=1, relief="sunken",
+    justify="left")
+my_label2.pack(pady=20, ipady=10, ipadx=10)
+
+my_label3 = Label(root,
+    text="Brian\nBrian Brian\nBrian Brian Brian",
+    font=("Comic Sans", 18),
+    bd=1, relief="sunken",
+    justify="right")
+my_label3.pack(pady=20, ipady=10, ipadx=10)
 
 
 root.mainloop()
