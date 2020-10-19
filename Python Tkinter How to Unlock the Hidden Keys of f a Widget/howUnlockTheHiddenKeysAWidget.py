4e59360ade12e294662866e139eda84669cf2ee9
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-19 16:21:45 -0500

    feat # Proyecto Web XI complete
    royecto Web X complete
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter How to Unlock the Hidden Keys of f a Widget/howUnlockTheHiddenKeysAWidget.py
@@ -0,0 +1,11 @@
+# Python Tkinter How to Unlock the Hidden Keys of f a Widget
+# Python Tkinter Cómo desbloquear las claves ocultas de un widget
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter How to Unlock the Hidden Keys of f a Widget")
+root.iconbitmap("Python Tkinter How to Unlock the Hidden Keys of f a Widget/icons/target.ico")
+root.geometry("550x550")
+
+root.mainloop()
