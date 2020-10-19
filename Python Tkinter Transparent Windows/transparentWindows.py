0f2af19f1151690603bbdc9937d1e73467bb4066
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-19 16:49:02 -0500

    feat Python Tkinter Transparent Windows start
    Python Tkinter Transparent Windows start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Transparent Windows/transparentWindows.py
@@ -0,0 +1,11 @@
+# Python Tkinter Transparent Windows
+# 
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter Transparent Windows")
+root.iconbitmap("Python Tkinter Transparent Windows/icons/target.ico")
+root.geometry("550x550")
+
+root.mainloop()
