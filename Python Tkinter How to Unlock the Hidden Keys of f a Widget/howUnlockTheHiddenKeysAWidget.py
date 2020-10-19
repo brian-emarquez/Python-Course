3107469ef1406d36b52e6389c9ec6cf4b46550ea
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-19 16:45:41 -0500

    feat Python Tkinter How to Unlock the Hidden Keys of f a Widget complete
    Python Tkinter How to Unlock the Hidden Keys of f a Widget complete
    
    resolvesr: #
    se also: #

--- Python Tkinter How to Unlock the Hidden Keys of f a Widget/howUnlockTheHiddenKeysAWidget.py
+++ Python Tkinter How to Unlock the Hidden Keys of f a Widget/howUnlockTheHiddenKeysAWidget.py
@@ -8,4 +8,19 @@ root.title("Python Tkinter How to Unlock the Hidden Keys of f a Widget")
 root.iconbitmap("Python Tkinter How to Unlock the Hidden Keys of f a Widget/icons/target.ico")
 root.geometry("550x550")
 
+my_label = Label(root, text="My Label", font=("Helvetica", 15))
+my_label.pack(pady=20)
+
+my_entry = Entry(root)
+my_entry.pack(pady=20)
+
+#for key in my_label.keys():
+for key in my_entry.keys():
+    print (key)
+
+print(my_label.keys())
+
+print(my_label['text'])
+
+
 root.mainloop()
