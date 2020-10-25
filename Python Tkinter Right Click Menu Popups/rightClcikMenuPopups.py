6aeb9e4deadd4965eb172753b846927828d2dd1e
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-25 13:31:48 -0500

    feat Python Tkinter Right Click Menu Popups start
    Python Tkinter Right Click Menu Popups start
    
    resolve:
    see also:

---
+++ Python Tkinter Right Click Menu Popups/rightClcikMenuPopups.py
@@ -0,0 +1,14 @@
+# Python Tkinter Right Click Menu Popups!
+# ¡Ventanas emergentes del menú contextual de Python Tkinter!
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Right Click Menu Popups!')
+root.iconbitmap('Python Tkinter Right Click Menu Popups/icons/pay.ico')
+root.geometry("400x400") 
+
+
+
+
+root.mainloop()
