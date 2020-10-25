ba0e4b858048ae302ef8c53cb30ed67a2b6e92f8
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-25 15:32:05 -0500

    feat Python Tkinter Right Click Menu Popups complete
    Python Tkinter Right Click Menu Popups complete
    
    resolve:
    see also:

--- Python Tkinter Right Click Menu Popups/rightClcikMenuPopups.py
+++ Python Tkinter Right Click Menu Popups/rightClcikMenuPopups.py
@@ -6,9 +6,27 @@ from tkinter import *
 root = Tk()
 root.title('Python Tkinter Right Click Menu Popups!')
 root.iconbitmap('Python Tkinter Right Click Menu Popups/icons/pay.ico')
-root.geometry("400x400") 
+root.geometry("500x550") 
 
+my_label = Label(root, text="", font=("Helvetica", 20))
+my_label.pack(pady=20)
 
+def hello():
+    my_label.config(text="Hello Tkinter")
 
+def bye():
+     my_label.config(text="Bye Tkinter")
+
+def my_popup(e):
+    my_menu.tk_popup(e.x_root, e.y_root)
+
+# Create a Menu
+my_menu = Menu(root, tearoff=False)
+my_menu.add_command(label="Say Hello", command=hello)
+my_menu.add_command(label="Say Goodbye", command=bye)
+my_menu.add_separator()
+my_menu.add_command(label="Exit", command=root.quit)
+
+root.bind("<Button-3>", my_popup)
 
 root.mainloop()
