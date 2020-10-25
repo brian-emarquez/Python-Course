b161d046789fca9502fc2579e78fa433608ddbdb
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-25 16:46:33 -0500

    feat Python Tkinter How To ser Tab Order and Focus start
    Python Tkinter How To ser Tab Order and Focus start
    
    resolve:
    see also:

---
+++ Python Tkinter How To ser Tab Order and Focus/howToSetTabOrderFocus.py
@@ -0,0 +1,14 @@
+# Python Tkinter How To ser Tab Order and Focus
+# Python Tkinter Cómo ser el orden de tabulación y el enfoque
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How To ser Tab Order and Focus!')
+root.iconbitmap('Python Tkinter How To ser Tab Order and Focus/icons/black.ico')
+root.geometry("500x550") 
+
+
+
+
+root.mainloop()
