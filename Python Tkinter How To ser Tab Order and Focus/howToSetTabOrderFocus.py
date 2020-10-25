bdccd761d2e6d8df961c15535677aef35e55ea73
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-25 17:32:52 -0500

    feat Python Tkinter How To ser Tab Order and Focus complete
    Python Tkinter How To ser Tab Order and Focus complete
    
    resolve:
    see also:

--- Python Tkinter How To ser Tab Order and Focus/howToSetTabOrderFocus.py
+++ Python Tkinter How To ser Tab Order and Focus/howToSetTabOrderFocus.py
@@ -9,6 +9,32 @@ root.iconbitmap('Python Tkinter How To ser Tab Order and Focus/icons/black.ico')
 root.geometry("500x550") 
 
 
+# Create Some entry boxes
+
+red = Entry(root, bg="red", font=("Helvetica", 20))
+white = Entry(root, bg="white", font=("Helvetica", 20))
+blue = Entry(root, bg="blue", font=("Helvetica", 20))
+
+# Pack item
+red.pack(pady=20)
+white.pack(pady=20)
+blue.pack(pady=20)
+
+# Pick focus
+white.focus() # posicion donde se empiesa a escribir
+
+# Change Tab Order
+def tab_order():
+    blue.focus()
+    widgets = [blue, white, red]
+    for w in widgets:
+        w.lift()
+
+
+my_button =  Button(root, text= "Change Tab Order", command=tab_order)
+my_button.pack(pady=20)
+
+tab_order()
 
 
 root.mainloop()
