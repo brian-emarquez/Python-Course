449b6e2700f9d926ea946335a1c17243fab27858
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-27 13:43:38 -0500

    feat Python Tkinter how To Create a Splash Screen start
    Python Tkinter how To Create a Splash Screen start
    
    resolve:
    see also:

---
+++ Python Tkinter how To Create a Splash Screen/howToCreateaSplashScreen.py
@@ -0,0 +1,13 @@
+#  Python Tkinter how To Create a Splash Screen
+#  Python Tkinter cómo crear una pantalla de bienvenida
+
+from tkinter import *
+
+root = Tk()
+root.title("Python tkinter How to Disable or Delete a Menu item")
+root.iconbitmap("Python Tkinter how To Create a Splash Screen/icons/cat.ico")
+root.geometry('500x500')
+
+
+
+root.mainloop()
