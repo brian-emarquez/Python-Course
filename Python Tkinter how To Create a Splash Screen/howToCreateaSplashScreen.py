313c2192b2fc3679da940fdc6513528a9c55f57e
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-27 18:51:25 -0500

    feat Python Tkinter how To Create a Splash Screen complete
    Python Tkinter how To Create a Splash Screen complete
    
    resolve:
    see also:

--- Python Tkinter how To Create a Splash Screen/howToCreateaSplashScreen.py
+++ Python Tkinter how To Create a Splash Screen/howToCreateaSplashScreen.py
@@ -3,11 +3,28 @@
 
 from tkinter import *
 
-root = Tk()
-root.title("Python tkinter How to Disable or Delete a Menu item")
-root.iconbitmap("Python Tkinter how To Create a Splash Screen/icons/cat.ico")
-root.geometry('500x500')
+splash_root = Tk()
+splash_root.title("Splash Screen!!")
+splash_root.geometry("300x200+-50+250")
+# Hoide the itle bar
+splash_root.overrideredirect(True)
+
+spash_label = Label(splash_root, text="Splash Screen", font=("Helvetica", 18))
+spash_label.pack(pady=20)
+
+def main_window():
+    splash_root.destroy()
+
+    root = Tk()
+    root.title("Python tkinter How to Disable or Delete a Menu item")
+    root.iconbitmap("Python Tkinter how To Create a Splash Screen/icons/cat.ico")
+    root.geometry("300x200+-50+250")
+
+    main_label = Label(root, text="Splash Screen", font=("Helvetica", 18))
+    main_label.pack(pady=20)
+
+# Spash Screnn Timer
+splash_root.after(1500, main_window)
 
 
-
-root.mainloop()
+mainloop()
