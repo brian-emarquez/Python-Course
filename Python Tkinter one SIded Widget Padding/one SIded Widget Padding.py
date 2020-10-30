3aaaf8d3c678375d264d3b5a9447ec69a74fe275
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-30 11:06:59 -0500

    feat Python Tkinter one SIded Widget Padding start
    Python Course Python Tkinter one SIded Widget Padding start
    
    resolve:
    see also:

---
+++ Python Tkinter one SIded Widget Padding/one SIded Widget Padding.py
@@ -0,0 +1,16 @@
+
+# Python Tkinter one SIded Widget Padding
+# Relleno de widgets a una cara
+
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter one SIded Widget Padding')
+root.iconbitmap('Python Tkinter one SIded Widget Padding/icons/among_us.ico')
+root.geometry("400x600") 
+
+
+
+root.mainloop()
