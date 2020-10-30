b9d59edd07ed8bb527d8151103d2c1a13247bd1d
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-30 11:25:15 -0500

    feat Python Tkinter one SIded Widget Padding complete
    Python Course Python Tkinter one SIded Widget Padding complete
    
    resolve:
    see also:

--- Python Tkinter one SIded Widget Padding/one SIded Widget Padding.py
+++ Python Tkinter one SIded Widget Padding/one SIded Widget Padding.py
@@ -9,8 +9,23 @@ from tkinter import *
 root = Tk()
 root.title('Python Tkinter one SIded Widget Padding')
 root.iconbitmap('Python Tkinter one SIded Widget Padding/icons/among_us.ico')
-root.geometry("400x600") 
+root.geometry("500x550") 
+root.config(bg="blue")
 
+# Create Label
+my_label = Label(root, text="Hello World!",
+    bg="white",
+    fg="black",
+    font=("Helvetica", 20))
 
+my_label.grid(row=0, column=0, pady=50, padx=(0,50))
+
+# Create a second label
+my_label2 = Label(root, text="Hello World 2!",
+    bg="white",
+    fg="black",
+    font=("Helvetica", 20))
+
+my_label2.grid(row=0, column=1)
 
 root.mainloop()
