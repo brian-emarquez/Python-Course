72862f8ce32aa85c992454b2186dfa93c7145274
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-04 18:53:29 -0500

    QT Designer Update

--- QT-Formulario/qtTestUI.py
+++ QT-Formulario/qtTestUI.py
@@ -1,9 +1,17 @@
 from PySide2 import QtWidgets
 
-from ui import formulario
+import formulario
+
 
 class MyQtApp(formulario.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self):
-        super(MyQtApp).__init__(self)
+        super(MyQtApp, self).__init__()
         self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+    qt_app = MyQtApp()
+    qt_app.show()
+    app.exec_()
+
 
