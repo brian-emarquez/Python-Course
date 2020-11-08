2cc2b6d275ad715dfd663f597601d0ffce59593a
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-07 20:08:04 -0500

    feat Python Tkinter Tile Matching Game
     Python Tkinter Tile Matching Game
    
    resolve:
    see also:

---
+++ Python Tkinter Tile Matching Game/TileMatchingGame.py
@@ -0,0 +1,14 @@
+# Python Tkinter Tile Matching Game
+# Juego de combinación de mosaicos Python Tkinter
+
+from tkinter import *
+import time
+
+root = Tk()
+root.title('Python Tkinter Tile Matching Game')
+root.iconbitmap('Python Tkinter Tile Matching Game/icons/icons.ico')
+root.geometry("600x400") 
+
+
+
+root.mainloop()
