83755d3faa72d5d3bfab4a83fcd6d2ae4d389f53
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-07 23:18:28 -0500

    feat Python tile matching game complete
    Python tile matching game complete
    
    resolves:
    see also:

--- Python Tkinter Tile Matching Game/TileMatchingGame.py
+++ Python Tkinter Tile Matching Game/TileMatchingGame.py
@@ -2,6 +2,7 @@
 # Juego de combinación de mosaicos Python Tkinter
 
 from tkinter import *
+from tkinter import messagebox
 import random
 
 root = Tk()
@@ -17,6 +18,44 @@ count = 0
 answer_list = []
 answer_dict = {}
 
+#Function fir cliching buttons
+def button_click(b, number):
+    global count, answer_list, answer_dict  
+
+    if b["text"] == ' ' and count < 2:
+        b["text"] = matches[number]
+        #add number to answer list
+        answer_list.append(number)
+        # add button and number to Answer Dictionary
+        answer_dict[b] = matches[number]
+        # Increment our Counter
+        count += 1
+        print(answer_list)
+
+    #Start too determine correct or not
+    if len(answer_list) == 2:
+        if matches[answer_list[0]] == matches[answer_list[1]]:
+            my_label.config(text="MATCH!")
+            for Key in answer_dict:
+                Key["state"] = "disable"
+            count = 0
+            answer_list = []
+            answer_dict = {}
+        else:
+            my_label.config(text="DOH!")
+            count = 0
+            answer_list = []
+            # POP UP BOX
+            messagebox.showinfo("Incorrect", "Incorrect")
+
+            # Reset the Buttons
+            for Key in answer_dict:
+                Key["test"] = " "
+
+            answer_dict = {}
+
+
+
 # Shuffle our matches
 random.shuffle(matches)
 
@@ -25,14 +64,6 @@ random.shuffle(matches)
 #Create button Frame
 my_frame = Frame(root)
 my_frame.pack(pady=10)
-
-#Function fir cliching buttons
-def button_click(b, number):
-    global count, answer_list, answer_dict  
-
-    if b["text"] == ' ' and count < 2:
-        b["text"] = matches[number]
-
 
 #Define out buttons
 b0 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b0, 0))
@@ -63,5 +94,8 @@ b8.grid(row=2, column=0)
 b9.grid(row=2, column=1)
 b10.grid(row=2, column=2)
 b11.grid(row=2, column=3)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
 
 root.mainloop()
