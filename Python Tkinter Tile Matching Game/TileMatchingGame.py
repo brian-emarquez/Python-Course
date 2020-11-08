c02648672aa06ac0aac9f0463d8156bf7dee2749
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-07 22:22:02 -0500

    feat Python tile matching game lambda
    Python tile matching game lambda
    
    resolves:
    see also:

--- Python Tkinter Tile Matching Game/TileMatchingGame.py
+++ Python Tkinter Tile Matching Game/TileMatchingGame.py
@@ -12,6 +12,11 @@ root.geometry("500x550")
 #create our matches
 matches = [1,1,2,2,3,3,4,4,5,5,6,6] 
 
+# Define some VARIABLES
+count = 0
+answer_list = []
+answer_dict = {}
+
 # Shuffle our matches
 random.shuffle(matches)
 
@@ -21,23 +26,27 @@ random.shuffle(matches)
 my_frame = Frame(root)
 my_frame.pack(pady=10)
 
-#Functions 
-def something():
-    pass
+#Function fir cliching buttons
+def button_click(b, number):
+    global count, answer_list, answer_dict  
+
+    if b["text"] == ' ' and count < 2:
+        b["text"] = matches[number]
+
 
 #Define out buttons
-b0 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b1 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b2 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b3 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b4 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b5 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b6 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b7 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b8 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b9 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b10 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
-b11 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=something)
+b0 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b0, 0))
+b1 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b1, 1))
+b2 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b2, 2))
+b3 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b3, 3))
+b4 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b4, 4))
+b5 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b5, 5))
+b6 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b6, 6))
+b7 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b7, 7))
+b8 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b8, 8))
+b9 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b9, 9))
+b10 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b10, 10))
+b11 = Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda : button_click(b11, 11))
 
 #Grid out Buttons
 b0.grid(row=0, column=0)
@@ -54,10 +63,5 @@ b8.grid(row=2, column=0)
 b9.grid(row=2, column=1)
 b10.grid(row=2, column=2)
 b11.grid(row=2, column=3)
-
-
-
-
-
 
 root.mainloop()
