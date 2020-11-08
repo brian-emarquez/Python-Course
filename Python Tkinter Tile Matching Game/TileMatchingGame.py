eaec8e9211e4a6ab9d497244834b84ae01d3ba1a
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-07 21:50:04 -0500

    feat Python tile matching game update
    Python tile matching game update
    
    resolves:
    see also:

--- Python Tkinter Tile Matching Game/TileMatchingGame.py
+++ Python Tkinter Tile Matching Game/TileMatchingGame.py
@@ -2,13 +2,19 @@
 # Juego de combinación de mosaicos Python Tkinter
 
 from tkinter import *
-import time
+import random
 
 root = Tk()
 root.title('Python Tkinter Tile Matching Game')
 root.iconbitmap('Python Tkinter Tile Matching Game/icons/icons.ico')
-root.geometry("600x400") 
+root.geometry("500x550") 
 
+#create our matches
+matches = [1,1,2,2,3,3,4,4,5,5,6,6] 
 
+# Shuffle our matches
+random.shuffle(matches)
+
+print(matches)
 
 root.mainloop()
