8ff6e82ab48c0cdcdd73a818dc2f9919ed438a65
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-10 17:44:23 -0500

    Python Tkinter How to read a PDF fILE Create

---
+++ Python Tkinter How To Read A PDF File/HowTo ReadAPDFfile.py
@@ -0,0 +1,15 @@
+# Python Tkinter How To Read A PDF File
+# Python Tkinter Cómo leer un archivo PDF
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter How To Read A PDF File")
+root.iconbitmap("Python Tkinter How To Read A PDF File/icons/adobe.ico")
+root.geometry('500x350')
+
+
+
+
+
+root.mainloop()
