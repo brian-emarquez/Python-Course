11478e3d4c1a790e2f08edcd1c05c826092712ca
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-15 23:08:11 -0500

    feat Python Tkinter Python Tkinter Rock Paper Scissors Game start
    Python Tkinter Python Tkinter Rock Paper Scissors Game start
    
    resolvesr: #
    se also: #

--- Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
+++ Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------------#
-# Python Tkinter Rock Paper Scissors Game'
+# Python Tkinter Rock Paper Scissors Game
 # Python Tkinter Rock Paper Scissors Juego
 #-----------------------------------------------------------------------------------#
 
@@ -9,6 +9,9 @@ root = Tk()
 root.title('Python Tkinter Rock Paper Scissors Game')
 root.iconbitmap('Python Tkinter Rock Paper Scissors Game/icons/joystick.ico')
 root.geometry("500x350") 
+
+
+
 
 
 root.mainloop()
