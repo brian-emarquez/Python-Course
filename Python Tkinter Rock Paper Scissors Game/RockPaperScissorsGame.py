ca796f47f42e0479ceab1bfa5b64f0e8d7a1ef58
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-15 23:49:39 -0500

    feat Python Tkinter Python Tkinter Rock Paper Scissors Game READMEE
    Python Tkinter Python Tkinter Rock Paper Scissors Game  READMEE
    
    resolvesr: #
    se also: #

--- Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
+++ Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
@@ -4,6 +4,7 @@
 #-----------------------------------------------------------------------------------#
 
 from tkinter import *
+from random import randint
 
 root = Tk()
 root.title('Python Tkinter Rock Paper Scissors Game')
@@ -12,6 +13,22 @@ root.geometry("500x600")
 
 # Change bg color to white
 root.config(bg="white")
+
+# Define our images
+rock = PhotoImage(file='Python Tkinter Rock Paper Scissors Game/images/rock.png')
+#oImage(file='Python Tkinter Rock Paper Scissors Game/images/paper.jpg')
+scissors = PhotoImage(file='Python Tkinter Rock Paper Scissors Game/images/scissor.png')
+
+# Add Images to a list
+image_list = [root,  scissors]
+
+# Pick random number between 0 and 2
+pick_number = randint(0,2)
+
+#throw uo an image when the program starts
+image_label = Label(root, image=image_list[pick_number])
+image_label.pack(pady=20)
+
 
 
 root.mainloop()
