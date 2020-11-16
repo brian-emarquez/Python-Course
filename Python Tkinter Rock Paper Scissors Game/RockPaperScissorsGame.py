74488a0684896d93f2466a4d5be87c4bbd81a496
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-15 23:06:20 -0500

    first commit

---
+++ Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------------------------------------#
+# Python Tkinter Rock Paper Scissors Game'
+# Python Tkinter Rock Paper Scissors Juego
+#-----------------------------------------------------------------------------------#
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Rock Paper Scissors Game')
+root.iconbitmap('Python Tkinter Rock Paper Scissors Game/icons/joystick.ico')
+root.geometry("500x350") 
+
+
+root.mainloop()
