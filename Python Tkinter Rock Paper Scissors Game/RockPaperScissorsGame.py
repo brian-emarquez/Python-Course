2fc683c8f6279c32e3150c1b609ca32eb4f02646
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-16 11:14:28 -0500

    feat Python Course - Python Tkinter Rock Papper Scissons Gome if
    Python Course - Python Tkinter Rock Papper Scissons Gome if
    
    resolves:
    see also:

--- Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
+++ Python Tkinter Rock Paper Scissors Game/RockPaperScissorsGame.py
@@ -5,6 +5,7 @@
 
 from tkinter import *
 from random import randint
+from tkinter import ttk
 
 root = Tk()
 root.title('Python Tkinter Rock Paper Scissors Game')
@@ -31,14 +32,40 @@ image_label.pack(pady=20)
 
 # Create Spin Function
 def spin():
+    # Pick random number
     pick_number = randint(0,2)
+    #show image
     image_label.config(image=image_list[pick_number])
+
+    # 0 = rock
+    # 1 = Papper
+    # 2 = scissors
+    # convert dropdown choice to a number
+
+    if user_choice.get() == "Rock":
+        user_choice_value = 0
+    elif user_choice.get() == "Paper":
+        user_choice_value = 1
+    elif user_choice.get() == "Scissors":
+        user_choice_value = 2
+
+    # Determine if we won or lost
+    if user_choice_value ==0: #Rock
+
+
+
+# Make  our choice
+user_choice = ttk.Combobox(root, value=("Rock", "Paper", "Scissors"))
+user_choice.current(0)
+user_choice.pack(pady=20)
 
 # create spin button
 spin_button = Button(root, text="Spin!", command=spin)
 spin_button.pack(pady=10)
 
-
+# Label for showing
+win_lose_label = Label(root, text="", font=("Helvetica", 18))
+win_lose_label.pack(pady=50)
 
 
 root.mainloop()
