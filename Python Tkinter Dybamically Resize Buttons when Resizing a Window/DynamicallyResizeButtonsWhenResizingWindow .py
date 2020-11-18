0d1c301831e97f138da697452416306f59c06f70
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-18 11:30:24 -0500

    feat Python Tkinter Dybamically Resize Buttons when Resizing a Window start
    Python Tkinter Dybamically Resize Buttons when Resizing a Window start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Dybamically Resize Buttons when Resizing a Window/DynamicallyResizeButtonsWhenResizingWindow .py
@@ -0,0 +1,16 @@
+###############################################################################################
+# Python Tkinter Dybamically Resize Buttons when Resizing a Window
+# Python Tkinter redimensiona dinámicamente los botones al redimensionar una ventana
+#############################################################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Dybamically Resize Buttons when Resizing a Window')
+root.iconbitmap('Python Tkinter Dybamically Resize Buttons when Resizing a Window/icons/theapplication.ico')
+root.geometry("400x600") 
+
+
+
+
+root.mainloop()
