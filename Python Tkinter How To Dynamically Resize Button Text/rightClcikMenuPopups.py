5c21b314231d4298f9fac3014cbf1045dbc63877
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-19 15:25:24 -0500

    feat Python Tkinter How To Dynamically Resize Button Text start
    Python Tkinter How To Dynamically Resize Button Text start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter How To Dynamically Resize Button Text/rightClcikMenuPopups.py
@@ -0,0 +1,15 @@
+##########################################################################
+#  Python Tkinter How To Dynamically Resize Button Text
+# Python Tkinter Cómo cambiar el tamaño del texto del botón de forma dinámica
+##########################################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How To Dynamically Resize Button Text!')
+root.iconbitmap('Python Tkinter How To Dynamically Resize Button Text/icons/back.ico')
+root.geometry("500x550") 
+
+
+
+root.mainloop()
