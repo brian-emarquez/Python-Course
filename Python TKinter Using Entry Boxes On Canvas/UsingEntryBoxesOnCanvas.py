5a759f89638b236e92b4d674f7b0f485403b2e31
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-04 17:53:46 -0500

    feat Python TKinter Using Entry Boxes On Canvas
    Python TKinter Using Entry Boxes On Canvas start
    
    also:
    see:

---
+++ Python TKinter Using Entry Boxes On Canvas/UsingEntryBoxesOnCanvas.py
@@ -0,0 +1,14 @@
+####################################################################
+# Python TKinter Using Entry Boxes On Canvas
+# Python TKinter usando cuadros de entrada en lienzo
+####################################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('Python TKinter Using Entry Boxes On Canvas')
+root.iconbitmap('Python TKinter Using Entry Boxes On Canvas/icons/canvas.ico')
+root.geometry("400x400") 
+
+
+root.mainloop()
