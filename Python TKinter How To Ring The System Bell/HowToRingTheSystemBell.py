a55bfe0b0154d9898b00246a4794eb399d0a2186
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-04 22:53:53 -0500

    feat Python TKinter How To Ring The System Bell start
    Python TKinter How To Ring The System Bell start
    
    resolvesr: #
    se also: #

---
+++ Python TKinter How To Ring The System Bell/HowToRingTheSystemBell.py
@@ -0,0 +1,17 @@
+####################################################################
+# Python TKinter How To Ring The System Bell
+# Python TKinter Cómo tocar la campana del sistema
+####################################################################
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Python TKinter How To Ring The System Bell')
+root.iconbitmap('Python TKinter How To Ring The System Bell/icons/bell.ico')
+root.geometry("323x576") 
+
+
+
+
+root.mainloop()
