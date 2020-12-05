e7b1de0648248e22dceaec19967e65d36901a31f
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-04 22:40:13 -0500

    feat Python TKinter Using Entry Boxes On Canvas complete
    Python TKinter Using Entry Boxes On Canvas complete
    
    resolvesr: #
    se also: #

--- Python TKinter Using Entry Boxes On Canvas/UsingEntryBoxesOnCanvas.py
+++ Python TKinter Using Entry Boxes On Canvas/UsingEntryBoxesOnCanvas.py
@@ -10,6 +10,8 @@ root = Tk()
 root.title('Python TKinter Using Entry Boxes On Canvas')
 root.iconbitmap('Python TKinter Using Entry Boxes On Canvas/icons/canvas.ico')
 root.geometry("323x576") 
+#make sure app
+root.resizable(width=False, height=False)
 
 # Define background Image
 bg = ImageTk.PhotoImage(file="Python TKinter Using Entry Boxes On Canvas/images/background.jpg")
@@ -25,14 +27,41 @@ my_canvas.create_image(0,0, image=bg, anchor="nw")
 un_entry = Entry(root,font=("Helvetica", 24), width=14, fg="#336d92", bd=0)
 pw_entry = Entry(root,font=("Helvetica", 24), width=14, fg="#336d92", bd=0)
 
-# Add the entry boxes to the canvas
 
+un_entry.insert(0, "username") 
+pw_entry.insert(0, "password") 
+
+# Add the entry boxes to the canvas
 un_window = my_canvas.create_window(34, 290, anchor="nw", window=un_entry)
 pw_window = my_canvas.create_window(34, 370, anchor="nw", window=pw_entry)
 
+#Define function Welcome
+def welcome():
+    un_entry.destroy()
+    pw_entry.destroy()
+    login_btn.destroy()
+
+    # Add a welcome message
+    my_canvas.create_text(160, 450, text="welcome", font=("Helvetica", 40), fill="white" )
+
+
 # Define Button
-login_bt = Button(root, text="Login", font=("Helvetica", 20), width=15, fg="#336d92")
-login
+login_btn = Button(root, text="Login", font=("Helvetica", 20), width=15, fg="#336d92", command=welcome)
+login_btn_window = my_canvas.create_window(36, 470, anchor="nw", window=login_btn)
+
+
+# Define entry_Clear function
+def entry_clear(e):
+    if un_entry.get() == 'username' or pw_entry.get() == 'password':
+        un_entry.delete(0,END)
+        pw_entry.delete(0,END)
+        # change text t o starts
+        pw_entry.config(show="*")
+
+
+# Bind the entry boxes
+un_entry.bind("<Button-1>", entry_clear)
+un_entry.bind("<Button-1>", entry_clear)
 
 
 
