44c4207edfa5fdbb73ee1ea3e06d04f8b8df9109
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-04 23:11:48 -0500

    feat Python TKinter How To Ring The System Bell complete
    Python TKinter How To Ring The System Bell complete
    
    resolvesr: #
    se also: #

--- Python TKinter How To Ring The System Bell/HowToRingTheSystemBell.py
+++ Python TKinter How To Ring The System Bell/HowToRingTheSystemBell.py
@@ -4,14 +4,30 @@
 ####################################################################
 
 from tkinter import *
-from PIL import ImageTk, Image
 
 root = Tk()
 root.title('Python TKinter How To Ring The System Bell')
 root.iconbitmap('Python TKinter How To Ring The System Bell/icons/bell.ico')
-root.geometry("323x576") 
+root.geometry("500x500") 
 
+# Define image
+bell = PhotoImage(file="Python TKinter How To Ring The System Bell/images/bell.png")
 
+# add image to label
+bell_label = Label(root, image=bell)
+bell_label.pack(pady=20)
 
+def ring():
+    root.bell()
 
+# Create our button
+my_button  = Button(root,
+    text="Ring the Bell",
+    command=ring,
+    font=("Helvetica", 24),
+    fg="#4d4d4d")
+
+my_button.pack(pady=20)
+
+#root.bell()
 root.mainloop()
