3f4f802f57fbc77be049b3f9365c209349e0f4ee
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-14 18:15:15 -0500

    feat Python Tkinter How To Define Custom Fontsstart
    Python Tkinter How To Define Custom Fonts start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter How To Define Custom Fonts/HowToDefineCustomFonts.py
@@ -0,0 +1,18 @@
+##########################################################
+# Python Tkinter How To Define Custom Fonts
+# Python Tkinter Cómo definir fuentes personalizadas
+##########################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter How To Define Custom Fonts')
+root.iconbitmap('Python Tkinter How To Define Custom Fonts/icons/arrow.ico')
+root.geometry("400x400")
+
+
+
+
+
+
+root.mainloop()
