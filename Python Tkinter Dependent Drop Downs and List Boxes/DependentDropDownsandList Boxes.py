06ba9097d065c2907fab27b49da91989a147b157
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-14 18:11:36 -0500

    feat Python Tkinter Dependent Drop Downs and List Boxes
    Python Tkinter Dependent Drop Downs and List Boxes
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Dependent Drop Downs and List Boxes/DependentDropDownsandList Boxes.py
@@ -0,0 +1,18 @@
+##########################################################
+# Python Tkinter Dependent Drop Downs and List Boxes
+# Cuadros de lista y desplegables dependientes de Python Tkinter
+##########################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Dependent Drop Downs and List Boxes')
+root.iconbitmap('Python Tkinter Dependent Drop Downs and List Boxes/icons/arrow.ico')
+root.geometry("400x400")
+
+
+
+
+
+
+root.mainloop()
