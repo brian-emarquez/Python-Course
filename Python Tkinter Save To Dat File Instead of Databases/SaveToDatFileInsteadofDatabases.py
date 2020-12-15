184a6b610624a242382b8fb40567665640b7da18
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-15 15:45:43 -0500

    feat Python Tkinter Save To Dat File Instead of Databases update
    Python Tkinter Save To Dat File Instead of Databases update
    
    resolvesr: #
    se also: #

--- Python Tkinter Save To Dat File Instead of Databases/SaveToDatFileInsteadofDatabases.py
+++ Python Tkinter Save To Dat File Instead of Databases/SaveToDatFileInsteadofDatabases.py
@@ -4,12 +4,12 @@
 ##########################################################
 
 from tkinter import *
-from tkinter import ttk
+import pickle
 
 root = Tk()
 root.title('Python Tkinter Save To Dat File Instead of Databases')
 root.iconbitmap('Python Tkinter Save To Dat File Instead of Databases/icons/arrow.ico')
-root.geometry("500x500")
+root.geometry("500x400")
 
 # Creatinf a list of sizes
 sizes = [
@@ -18,78 +18,42 @@ sizes = [
     "Large"
 ]
 
-# Create a lsit of colors
-small_colors = [
-    "Red",
-    "Green",
-    "Blue",
-    "Black"
-]
-medium_colors = [
-    "Red",
-    "Green"]
+my_text = Text(root, width=20, height=10)
+my_text.pack(pady=20)
 
-large_colors = [
-    "Blue",
-    "Black"]
+def save_file():
+    #Grab the studdd from our text box
+    stuff = my_text.get(1.0, END)
 
-# show small_colors
-def pick_color(e):
-    if my_combo.get() == "Small":
-        color_combo.config(value=small_colors)
-        color_combo.current(0)
+    # Define a filename
+    filename = "Python Tkinter Save To Dat File Instead of Databases/data/dat_stuff"
 
-    if my_combo.get() == "Medium":
-        color_combo.config(value=medium_colors)
-        color_combo.current(0)
+    # Opem the file
+    output_file = open(filename, 'wb')
+    
+    # Actually add the data to the file
+    pickle.dump(stuff, output_file)
 
-    if my_combo.get() == "Large":
-        color_combo.config(value=large_colors)
-        color_combo.current(0)
 
-# Create a drop Box
-my_combo = ttk.Combobox(root, value=sizes)
-my_combo.current(0)
-my_combo.pack(pady=20)
+def Open_file():
+    # Define a filename
+    filename = "Python Tkinter Save To Dat File Instead of Databases/data/dat_stuff"
 
-# bind the conbobox
-my_combo.bind("<<ComboboxSelected>>", pick_color)
+    # Open the file
+    input_file = open(filename, 'rb')
 
-# Color combo box
-color_combo = ttk.Combobox(root, value=[" "])
-color_combo.current(0)
-color_combo.pack(pady=20)
+    #Load the data
+    stuff = pickle.load(input_file)
 
-# List Boxes
-my_frame = Frame(root)
-my_frame.pack(pady=50)
+    # output to text box
+    my_text.insert(1.0, stuff)
+    print(stuff)
 
-# List boxes
-my_list1 = Listbox(my_frame)
-my_list2 = Listbox(my_frame)
-my_list1.grid(row=0, column=0)
-my_list2.grid(row=0, column=1, padx=20)
+my_button1 = Button(root, text="Save File", command=save_file)
+my_button2 = Button(root, text="Save File", command=Open_file)
 
-def list_color(e):
-    my_list2.delete(0, END)
+my_button1.pack(pady=20)
+my_button2.pack(pady=20)
 
-    if my_list1.get(ANCHOR) == "Small":
-        for item in small_colors:
-            my_list2.insert(END, item)
-
-    if my_list1.get(ANCHOR) == "Medium":
-        for item in medium_colors:
-            my_list2.insert(END, item)
-
-    if my_list1.get(ANCHOR) == "Large":
-        for item in large_colors:
-            my_list2.insert(END, item)
-
-#add items to list1
-for item in sizes:
-    my_list1.insert(END, item)
-
-# Bind the Listbox
-my_list1.bind("<<ListboxSelect>>", list_color)
 
 root.mainloop()
