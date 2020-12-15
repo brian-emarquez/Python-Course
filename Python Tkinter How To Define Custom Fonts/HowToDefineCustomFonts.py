85354d78c708717e8f62e1835785445e98da0fbd
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-15 13:27:57 -0500

    feat Python Tkinter How To Define Custom Font update
    Python Tkinter How To Define Custom Fonts update
    
    resolvesr: #
    se also: #

--- Python Tkinter How To Define Custom Fonts/HowToDefineCustomFonts.py
+++ Python Tkinter How To Define Custom Fonts/HowToDefineCustomFonts.py
@@ -4,15 +4,37 @@
 ##########################################################
 
 from tkinter import *
+from tkinter.font import Font
 
 root = Tk()
 root.title('Python Tkinter How To Define Custom Fonts')
 root.iconbitmap('Python Tkinter How To Define Custom Fonts/icons/arrow.ico')
-root.geometry("400x400")
+root.geometry("500x500")
+
+# Define Out Font
+bigFont = Font(
+    family = "Tines",
+    size=42,
+    weight="bold",
+    slant="roman",
+    underline=0,
+    overstrike=0)
+
+mediumFont = Font(
+    family = "Helvetica",
+    size=24,
+    weight="normal",
+    slant="italic",
+    underline=1,
+    overstrike=0)
+
+#Define A butoon,
+my_button1 = Button(root, text="Big Text", font=bigFont)
+my_button1.pack(pady=20)
 
 
-
-
-
+# Label
+mylabel = Label (root, text="More Big Text", font=bigFont)
+mylabel.pack(pady=20)
 
 root.mainloop()
