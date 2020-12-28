41b8d7ab7dd12946fb6efc558a142c104a27a80f
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-28 16:36:59 -0500

    feat Python Tkinter All List I update
    Python Tkinter All List I update
    
    resolves:
    see also:

--- Python Tkinter All List I/allList.py
+++ Python Tkinter All List I/allList.py
@@ -3,13 +3,45 @@
 ##########################################################
 
 from tkinter import *
+from tkinter.font import Font
 
 root = Tk()
 root.title("Python Tkinter All List I")
 root.iconbitmap("Python Tkinter All List I/icons/drink.ico")
 root.geometry('500x350')
 
+# Define our Font
+my_font = Font(
+    family="Brush  Script MT Cursiva",
+    size =30,
+    weight="bold") 
 
-#
+my_frame = Frame(root)
+my_frame.pack(pady=10)
+
+# Create listbox
+my_list = Listbox(my_frame,
+    font=my_font,
+    width=25,
+    height=5,
+    bg="SystemButtonFace",
+    bd=0,
+    fg="#464646",
+
+)
+
+my_list.pack()
+
+# Create dummy list
+stuff = ["Walk the Dog", "Buy Griceries", "Take a Nap", "Learn Tkinter", "Rule the wold"]
+
+# add dummy list to list box
+for item in stuff:
+    my_list.insert(END, item)
+
+
+
+
+
 
 root.mainloop()
