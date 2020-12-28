ae9c21b82b0c3fddcd658310e78ad8cbd43dc722
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-28 18:23:14 -0500

    feat Python Tkinter All List III Save and Open ToDo Lists update
    Python Tkinter All List III Save and Open ToDo Lists update
    
    resolves:
    see also:

--- Python Tkinter All List III Save and Open ToDo Lists/SaveandOpe ToD Lists.py
+++ Python Tkinter All List III Save and Open ToDo Lists/SaveandOpe ToD Lists.py
@@ -1,7 +1,7 @@
-##########################################################
+############################################################################
 # Python Tkinter All List III Save and Open ToDo Lists
 # Python Tkinter All List III Guardar y abrir listas de tareas pendientes
-##########################################################
+############################################################################
 
 from tkinter import *
 from tkinter.font import Font
@@ -51,7 +51,7 @@ my_list.config(yscrollcommand=my_scrollbar.set)
 my_scrollbar.config(command=my_list.yview)
 
 # create entry box to add items to the list 
-my_entry = Entry(root, font=("Helvetica", 24))
+my_entry = Entry(root, font=("Helvetica", 24),width=26)
 my_entry.pack(pady=20)
 
 # Create a button frame
@@ -84,22 +84,45 @@ def uncross_item():
     # Get rid of selection bar
     my_list.select_clear(0, END)
 
+# Delete Selecttetd
 def delete_crossed():
     count = 0
     while count < my_list.size():
         if my_list.itemcget(count, "fg") == "#dedede":
             my_list.delete(my_list.index(count))
+        else:
+            count += 1
 
-        count += 1
-    
+def save_list():
+    pass
+
+def open_list():
+    pass
+
+def clear_list():
+    pass
+
+# Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+# Add items to the menu
+file_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="File", menu=file_menu)
+
+#addd dropdown items
+file_menu.add_command(label="Save List", command=save_list)
+file_menu.add_command(label="Open List", command=open_list)
+file_menu.add_separator()
+file_menu.add_command(label="Clear List", command=clear_list)
+
+
 # add some buttons
 delete_button = Button(button_frame, text="Delete Item", command=delete_item)
 add_button = Button(button_frame, text="Add Item", command=add_item)
 cross_off_button = Button(button_frame, text="Cross off Item", command=cross_off_item)
 uncross_button = Button(button_frame, text="Uncross Item", command=uncross_item)
 delete_crossed_button = Button(button_frame, text="Delete Crossed", command=delete_crossed)
-
-
 delete_button.grid(row=0, column=0)
 add_button.grid(row=0, column=1, padx=20)
 cross_off_button.grid(row=0, column=2)
