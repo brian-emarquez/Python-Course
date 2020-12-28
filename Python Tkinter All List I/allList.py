af24a6abce57f099e19ecd935ffdfbbeee255d5c
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-28 16:13:01 -0500

    feat Python Tkinter All List I start
    Python Tkinter All List I start
    
    resolves:
    see also:

---
+++ Python Tkinter All List I/allList.py
@@ -0,0 +1,13 @@
+##########################################################
+#  Python Tkinter All List I
+##########################################################
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter All List I")
+root.iconbitmap("Python Tkinter All List I/icons/drink.ico")
+root.geometry('500x350')
+
+
+root.mainloop()
