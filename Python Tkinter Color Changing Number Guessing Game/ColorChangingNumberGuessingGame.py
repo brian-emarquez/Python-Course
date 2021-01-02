39f8ef5a4305adb1413fca3b676a02cb5f190880
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-02 13:04:46 -0500

    feat Python Tkinter Color Changing Number Guessing Game start
    Python Tkinter Color Changing Number Guessing Game start
    
    resolvesr: #
    se also: #

---
+++ Python Tkinter Color Changing Number Guessing Game/ColorChangingNumberGuessingGame.py
@@ -0,0 +1,16 @@
+#######################################################################
+# Python Tkinter Color Changing Number Guessing Game
+# Python Tkinter Juego de adivinanzas de números que cambian de color
+########################################################################
+
+from tkinter import *
+from tkinter import colorchooser
+
+
+root = Tk()
+root.title('Python Tkinter Color Changing Number Guessing Game')
+root.geometry("400x400")
+root.iconbitmap('Python Tkinter Color Changing Number Guessing Game/hipopotamo.png')
+
+
+root.mainloop()
