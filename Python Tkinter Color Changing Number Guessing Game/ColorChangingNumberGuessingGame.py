d9ef068d0485e5cf9750ee027b5613cbbe38264d
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-02 13:19:59 -0500

    feat Python Tkinter Color Changing Number Guessing Game update funtions
    Python Tkinter Color Changing Number Guessing Game update funtions
    
    resolvesr: #
    se also: #

--- Python Tkinter Color Changing Number Guessing Game/ColorChangingNumberGuessingGame.py
+++ Python Tkinter Color Changing Number Guessing Game/ColorChangingNumberGuessingGame.py
@@ -4,13 +4,30 @@
 ########################################################################
 
 from tkinter import *
-from tkinter import colorchooser
-
+from random import randint
 
 root = Tk()
 root.title('Python Tkinter Color Changing Number Guessing Game')
-root.geometry("400x400")
-root.iconbitmap('Python Tkinter Color Changing Number Guessing Game/hipopotamo.png')
+root.iconbitmap('Python Tkinter Color Changing Number Guessing Game/icons/hipopotamo.png')
+root.geometry("500x500")
 
+num_label = Label(root, text="Pick A Number\nBetwen 1 and 10!", font=("Bush Script MT", 32))
+num_label.pack(pady=20)
+
+def guesser():
+    pass
+
+def rando():
+    pass
+
+
+guess_box = Entry(root, font=("Helvetica", 100), width=2)
+guess_box.pack(pady=20)
+
+guess_button = Button(root, text="Submit", command=guesser)
+guess_button.pack(pady=20)
+
+rand_button = Button(root, text="New Number", command=rando)
+rand_button.pack(pady=20)
 
 root.mainloop()
