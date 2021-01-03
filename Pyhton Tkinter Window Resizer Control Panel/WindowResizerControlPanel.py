c50156c537652d0a519589ff504de010897931f4
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-02 21:18:54 -0500

    feat Pyhton Tkinter Window Resizer Control Panel START
    Pyhton Tkinter Window Resizer Control Panel START
    
    resolvesr: #
    se also: #

---
+++ Pyhton Tkinter Window Resizer Control Panel/WindowResizerControlPanel.py
@@ -0,0 +1,16 @@
+###############################################################################
+# Pyhton Tkinter Window Resizer Control Panel
+# Panel de control de cambio de tamaño de ventana de Pyhton Tkinter
+###############################################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('Pyhton Tkinter Window Resizer Control Panel')
+root.iconbitmap('Pyhton Tkinter Window Resizer Control Panel/icons/resize.ico')
+root.geometry("500x500") 
+
+
+
+
+root.mainloop()
