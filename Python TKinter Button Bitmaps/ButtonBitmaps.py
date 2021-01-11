092c6bdfd27b59f09ecf188e0f2939311cc2380c
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-11 12:06:13 -0500

    feat Pyhton Tkinter Button Bitmaps complete
    Pyhton Tkinter Button Bitmaps complete
    
    also:
    see:

--- Python TKinter Button Bitmaps/ButtonBitmaps.py
+++ Python TKinter Button Bitmaps/ButtonBitmaps.py
@@ -8,7 +8,36 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title('Python TKinter Button Bitmaps')
 root.iconbitmap('Python TKinter Button Bitmaps/icons/elvis.ico')
-root.geometry("500x400") 
+root.geometry("300x900")
+
+'''
+error
+gray75
+gray50
+gray12
+hourglass
+info
+questhead
+question
+warning
+'''
+# Un solo bitmap
+#my_button = Button(root, bitmap="error", width=50, height=50)
+#my_button.pack(pady=50)
+
+list = ["error",
+"gray75",
+"gray50",
+"gray12",
+"hourglass",
+"info",
+"questhead",
+"question",
+"warning"]
+
+
+for map in list:
+    Button(root, bitmap=map, width=50, height=50, fg="darkblue").pack(pady=20)
 
 
 
