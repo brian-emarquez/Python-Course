1a43c8068766e63b8f4e60f747a7e54dfe585980
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-11 12:08:59 -0500

    feat Pyhton Tkinter  changing the mouse cursor start
    Pyhton Tkinter  changing the mouse cursor start
    
    also:
    see:

---
+++ Python TKinter Changing The Mouse Cursor/ChangingTheMouse Cursor.py
@@ -0,0 +1,15 @@
+######################################################################
+# Changing The Mouse Cursor
+###################################################################### 
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Python TKinter Changing The Mouse Cursor')
+root.iconbitmap('Python TKinter Changing The Mouse Cursor/icons/elvis.ico')
+root.geometry("500x500")
+
+
+
+root.mainloop()
