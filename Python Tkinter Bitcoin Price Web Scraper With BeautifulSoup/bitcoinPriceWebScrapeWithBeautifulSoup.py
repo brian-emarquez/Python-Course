fd7c7d191fcc6b974de055b79c28e15e0fb535af
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-19 16:24:19 -0500

    feat  Python Python Tkinter Bitcoin Price Web Scraper With BeautifulSoup start
    Python Python Tkinter Bitcoin Price Web Scraper With BeautifulSoup start
    
    Resolves: #
    See also: #

---
+++ Python Tkinter Bitcoin Price Web Scraper With BeautifulSoup/bitcoinPriceWebScrapeWithBeautifulSoup.py
@@ -0,0 +1,14 @@
+###########################################################################################
+#  Python Tkinter Bitcoin Price Web Scraper With BeautifulSoup
+# Raspador web de precios de Bitcoin de Python Tkinter con BeautifulSoup
+###########################################################################################
+
+from tkinter import *
+
+root = Tk()
+root.title('PPython Tkinter Bitcoin Price Web Scraper With BeautifulSoup')
+root.iconbitmap('Python Tkinter Bitcoin Price Web Scraper With BeautifulSoup/icons/bitcoin.ico')
+root.geometry("500x500")
+
+
+root.mainloop()
