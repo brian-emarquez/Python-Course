2c94ce7c271d797bea0982eb421a3994dc61a71b
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-19 15:40:41 -0500

    feat  Python tkinter basic search and Autofill update 2
    Python tkinter basic search and Autofill update 2
    
    Resolves: #
    See also: #

--- Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
+++ Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
@@ -10,6 +10,15 @@ root.title("Python Tkinter Basic Search and Autofill")
 root.iconbitmap("Python Tkinter Basic Search and Autofill/icons/fire.ico")
 root.geometry('500x300')
 
+# Update
+def update(data):
+    #clear tje listbox
+    my_list.delete(0, END)
+
+    # ADD TOPPING TO LISTBOX
+    for item in data:
+        my_list.insert(END, item)
+
 # Create Label
 my_label = Label(root, text="Start Typing...",
     font=("Helvetica", 14), fg="grey")
@@ -22,5 +31,16 @@ my_entry.pack()
 # Create a listbox
 my_list = Listbox(root, width=50)
 my_list.pack(pady=40)
+
+# Create a list of pizza topping
+topping = ["Peperoni", "Peppers", "Mushrooms", "Chesse", "Onions", "Ham", "Taco"]
+
+# Add the topping to our list
+update(topping)
+
+
+
+
+
 
 root.mainloop()
