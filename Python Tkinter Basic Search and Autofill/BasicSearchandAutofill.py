f64ba1d4a4aa6f63d2a0f146097d295436b27f7e
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-18 23:20:40 -0500

    feat Python Tkinter Basic Search and Autofill update
    Python Tkinter Basic Search and Autofill update
    
    Resolves: #
    See also: #

--- Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
+++ Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
@@ -8,8 +8,19 @@ from tkinter import *
 root = Tk()
 root.title("Python Tkinter Basic Search and Autofill")
 root.iconbitmap("Python Tkinter Basic Search and Autofill/icons/fire.ico")
-root.geometry('500x350')
+root.geometry('500x300')
 
+# Create Label
+my_label = Label(root, text="Start Typing...",
+    font=("Helvetica", 14), fg="grey")
+my_label.pack(pady=20)
 
+# Create an entry box
+my_entry = Entry(root, font=("Helvetica", 20))
+my_entry.pack()
+
+# Create a listbox
+my_list = Listbox(root, width=50)
+my_list.pack(pady=40)
 
 root.mainloop()
