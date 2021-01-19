f0f54f022648d4756c47c715e369d86bd4eacf89
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-18 21:44:52 -0500

    feat Python Tkinter On-Off Button Switch start
    Python Tkinter On-Off Button Switch start
    
    Resolves: #
    See also: #

---
+++ Python Tkinter On-Off Button Switch/on-offButtonSwitch.py
@@ -0,0 +1,15 @@
+##########################################
+# Python Tkinter ListBox
+########################################## 
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter On-Off Button Switch')
+root.iconbitmap('Python Tkinter On-Off Button Switch/icons/avatar.ico')
+root.geometry("400x400")
+
+
+
+root.mainloop()
