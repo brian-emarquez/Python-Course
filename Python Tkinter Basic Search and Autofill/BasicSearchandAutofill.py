f2a3139d5c22d5e770463973f99c9b5eda967b9d
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-19 16:17:21 -0500

    feat  Python tkinter basic search and Autofill complete
    Python tkinter basic search and Autofill complete
    
    Resolves: #
    See also: #

--- Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
+++ Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
@@ -19,6 +19,31 @@ def update(data):
     for item in data:
         my_list.insert(END, item)
 
+# Update entry box with listbox clicked
+def fillout(e):
+    # DELETE WHATEVER IS IN THE ENTRY BOX
+    my_entry.delete(0, END)
+
+    #add clicked list item to entry box
+    my_entry.insert(0, my_list.get(ACTIVE))
+
+# create function to check entry vs listbox
+def check(e):
+    #grab what was typed
+    typed = my_entry.get()
+
+    if typed =='':
+        data = toppings
+    else:
+        data = []
+        for item in toppings:
+            if typed.lower() in item.lower():
+                data.append(item)
+
+    # update our listbox with selected items
+    update(data)
+
+
 # Create Label
 my_label = Label(root, text="Start Typing...",
     font=("Helvetica", 14), fg="grey")
@@ -33,11 +58,16 @@ my_list = Listbox(root, width=50)
 my_list.pack(pady=40)
 
 # Create a list of pizza topping
-topping = ["Peperoni", "Peppers", "Mushrooms", "Chesse", "Onions", "Ham", "Taco"]
+toppings = ["Peperoni", "Peppers", "Mushrooms", "Chesse", "Onions", "Ham", "Taco"]
 
 # Add the topping to our list
-update(topping)
+update(toppings)
 
+# Create a binding on the listbox onclick
+my_list.bind("<<ListboxSelect>>", fillout)
+
+#CREATE A BINDING ON THE ENTRY BOX
+my_entry.bind("<KeyRelease>", check)
 
 
 
