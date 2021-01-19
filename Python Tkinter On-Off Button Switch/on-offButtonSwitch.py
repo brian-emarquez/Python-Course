fe3ab9fd7fce41379de656416370e920719d0728
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-18 22:43:10 -0500

    feat Python Tkinter On-Off Button Switch complete
    Python Tkinter On-Off Button Switch complete
    
    Resolves: #
    See also: #

--- Python Tkinter On-Off Button Switch/on-offButtonSwitch.py
+++ Python Tkinter On-Off Button Switch/on-offButtonSwitch.py
@@ -10,18 +10,36 @@ root.title('Python Tkinter On-Off Button Switch')
 root.iconbitmap('Python Tkinter On-Off Button Switch/icons/avatar.ico')
 root.geometry("1100x500")
 
+# Keep track of the button state on/off
+global is_on
+is_on = True
+
+
+
 my_label = Label(root, 
     text="The Swich Is on!", 
     fg="green", 
     font=("Helvetica",32))
 my_label.pack(pady=20)
 
+# Define our swith function
+def switch():
+    global is_on
+
+    # Determin is on off
+    if is_on:
+        on_button.config(image=off)
+        is_on = False
+    else:
+        on_button.config(image=on)
+        is_on = True
+    
 # Define our Imaages
 on = PhotoImage(file="Python Tkinter On-Off Button Switch/images/on.png")
 off = PhotoImage(file="Python Tkinter On-Off Button Switch/images/off.png")
 
 # Create A button
-on_button = Button(root, image=on)
+on_button = Button(root, image=on, bd=0, command=switch)
 on_button.pack(pady=50)
 
 root.mainloop()
