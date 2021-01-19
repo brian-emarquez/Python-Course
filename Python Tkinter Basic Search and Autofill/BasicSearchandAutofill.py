e1cfa21e3ee8364a1d7ce4e20ba759f10e580b6f
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-18 23:12:54 -0500

    feat Python Tkinter Basic Search and Autofill start
    Python Tkinter Basic Search and Autofill start
    
    Resolves: #
    See also: #

---
+++ Python Tkinter Basic Search and Autofill/BasicSearchandAutofill.py
@@ -0,0 +1,15 @@
+#############################################################
+#  Python Tkinter Basic Search and Autofill
+#############################################################
+
+from tkinter import *
+
+
+root = Tk()
+root.title("Python Tkinter Basic Search and Autofill")
+root.iconbitmap("Python Tkinter Basic Search and Autofill/icons/fire.ico")
+root.geometry('500x350')
+
+
+
+root.mainloop()
