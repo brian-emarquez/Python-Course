4edae69408f6730e1850b53d5cd8ba9802cd8817
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-02 13:29:10 -0500

    feat Python Tkinter How To Use The Message Widget For Blocks of Text  START
    Python Tkinter How To Use The Message Widget For Blocks of Text START
    
    Reseolver: #
    see also: #

---
+++ Python Tkinter How To Use The Message Widget For Blocks of Text/HowToUseTheMessageWidgetForBlocksofText.py
@@ -0,0 +1,13 @@
+##############################################################
+# How To Use The Message Widget For Blocks of Text
+#############################################################
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter How To Use The Message Widget For Blocks of Text")
+root.iconbitmap("Python Tkinter How To Use The Message Widget For Blocks of Text/icons/hulk.ico")
+root.geometry("500x400")
+
+
+root.mainloop()
