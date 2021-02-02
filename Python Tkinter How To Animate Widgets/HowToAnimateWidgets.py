01b8abe0c24bed7281f9c67caf1d3a1b691a3f4d
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-02 12:52:53 -0500

    feat Python Tkinter How To Animate Widgets START
    Python Tkinter How To Animate Widgets START
    
    Reseolver: #
    see also: #

---
+++ Python Tkinter How To Animate Widgets/HowToAnimateWidgets.py
@@ -0,0 +1,15 @@
+##############################################################
+# Python Tkinter How To Animate Widgets
+#############################################################
+
+from tkinter import *
+
+root = Tk()
+root.title("Python Tkinter How To Animate Widgets")
+root.iconbitmap("Python Tkinter How To Animate Widgets/icons/hulk.ico")
+
+
+
+
+
+root.mainloop()
