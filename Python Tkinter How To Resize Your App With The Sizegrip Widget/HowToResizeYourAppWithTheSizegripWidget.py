6c35b4b87de16e962e8dbd9456bf40a4f5a8d095
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-19 21:27:48 -0500

    feat Python Tkinter How To Resize Your App With The Sizegrip Widget START
    Python Tkinter How To Resize Your App With The Sizegrip Widget START
    
    resolves: #
    see also: #

---
+++ Python Tkinter How To Resize Your App With The Sizegrip Widget/HowToResizeYourAppWithTheSizegripWidget.py
@@ -0,0 +1,15 @@
+# HowToResizeYourAppWithTheSizegrip Widget
+# Cómo cambiar el tamaño de su aplicación con el widget de agarre de tamaño
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
+root = Tk()
+root.title('HowToResizeYourAppWithTheSizegripWidget')
+root.iconbitmap('Python Tkinter How To Resize Your App With The Sizegrip Widget/icons/document.ico')
+root.geometry("1000x660")
+
+
+
+root.mainloop()
