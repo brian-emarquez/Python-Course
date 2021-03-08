3cd62b556f8a52283712a40f0e60f63ed78385c8
Brian Marquez <brian3marquez@gmail.com>
2021-03-07 21:36:17 -0500

    Python Tkinter Build A Foreign Language Flashcard App
    Python Tkinter Build A Foreign Language Flashcard App
    
    see also: #
    resolves: #

---
+++ Python Tkinter Build A Foreign Language Flashcard App/BuildForeignLanguageFlashcardApp.py
@@ -0,0 +1,16 @@
+#############################################################################################
+#  Build A Foreign Language Flashcard App                                         ###########
+#############################################################################################
+
+from tkinter import *
+
+
+root = Tk()
+root.title('Python Tkinter Build A Foreign Language Flashcard App')
+root.iconbitmap('Python Tkinter Build A Foreign Language Flashcard App/icons/iconfinde.ico')
+root.geometry("550x210")
+root.config(bg="black")
+
+
+
+root.mainloop()
