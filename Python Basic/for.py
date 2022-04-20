fecb3248a76480d192991301ea853264ade4637e
BrianMarquez3 <brian3marquez@gmail.com>
2022-04-20 11:22:08 -0500

    for

---
+++ Python Basic/for.py
@@ -0,0 +1,9 @@
+# for
+
+cadena = 'Hola'
+
+for letra in cadena:
+    print(letra)
+else:
+    print('Fin ciclo for')
+    
---
+++ Python Basic/for2.py
@@ -0,0 +1,7 @@
+
+lis = [1, 2, 3, 4, 5]
+ 
+for i in range(len(lis)):
+     
+    print(lis[i])
+    
