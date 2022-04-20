888b240f3bbf5eda13bd59f5b6acc7560b2f7add
BrianMarquez3 <brian3marquez@gmail.com>
2022-04-20 10:54:24 -0500

    Operadort ternario

--- Python Basic/operadorTernario.py
+++ Python Basic/operadorTernario.py
@@ -2,10 +2,10 @@
 
 condicion = True
 
-if condicion:
+"""if condicion:
     print('La condicion es verdadera')  
 else: 
-    print('La condicion es falsa')
+    print('La condicion es falsa')"""
     
     
 print('condicion verdadea') if condicion else print('condicion falsa')
