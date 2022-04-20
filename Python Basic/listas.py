969113290d54e8f95e0d22cea58f56ff176d7169
BrianMarquez3 <brian3marquez@gmail.com>
2022-04-20 11:58:16 -0500

    listas

---
+++ Python Basic/listas.py
@@ -0,0 +1,7 @@
+# Listas
+
+nombres = ["Brian", "Edith", "Gines", "Roxana", "Mafer", "Rosario"]
+print(nombres)
+
+print(nombres[0])
+print(nombres[1])
---
+++ Python Basic/lsitas2.py
@@ -0,0 +1,11 @@
+# Listas 2
+
+nombres = ["Brian", "Edith", "Gines", "Roxana", "Mafer", "Rosario"]
+
+# Rango
+print(nombres[0:5])
+print(nombres[:4])
+
+# Cambiar elementos
+nombres[4] = "Demo"
+print(nombres)
