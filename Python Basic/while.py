6f68b3e062e0eb7279aa2118f1b2b28e9f5fe20b
BrianMarquez3 <brian3marquez@gmail.com>
2022-04-20 11:11:20 -0500

    while

---
+++ Python Basic/while.py
@@ -0,0 +1,13 @@
+# condicion = True
+#
+# while condicion:
+#     print('Ejecutando ciclo while')
+# else:
+#     print('Fin del ciclo while')
+
+contador = 0
+while contador <= 3:
+    print(contador)
+    contador += 1 #contador = contador + 1
+else:
+    print(f'se completo el ciclo while, contador ')
