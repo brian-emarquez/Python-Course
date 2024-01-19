f6e14cfa9eefcff8a126d3dc053585d64500b867
brianMarquez3 <brian3marquez@gmail.com>
2024-01-18 21:57:55 -0500

    ai-voice 2

---
+++ AI-Voice/voice2.py
@@ -0,0 +1,5 @@
+# This is the text that will be spoken.
+text = "hola esto es el curso de Python"
+
+# Pick a "preset mode" to determine quality. Options: {"ultra_fast", "fast" (default), "standard", "high_quality"}. See docs in api.py
+preset = "fast"
