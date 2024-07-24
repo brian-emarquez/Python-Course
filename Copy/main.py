2e68dd87d831ddfb9ffc42a4df89c769ae7eecea
brian-emarquez <brian3marquez@gmail.com>
2024-07-24 13:52:14 -0500

    Update

--- Copy/main.py
+++ Copy/main.py
@@ -13,22 +13,32 @@ year = obtener_ano_actual()
 fecha_anterior = datetime.now() - timedelta(days=1)
 nombre_generado = fecha_anterior.strftime("MIS%m%dALL")
 
-origen = 'C:\\BD\\Backups\\hola.txt'
+# Lista de archivos de origen
+archivos_origen = [
+    'C:\\DB\\Backups\\hola1.txt',
+    'C:\\DB\\Backups\\hola2.txt',
+    'C:\\DB\\Backups\\hola3.txt'
+]
 
-# Solicitar al usuario la ruta de la carpeta de destino
-destino = 'C:\\stokori\\db\\'+ year + mont + '\\' + nombre_generado + '\\' + 'hola.txt'
-
+# Ruta base de la carpeta de destino
+destino_base = f'C:\\stokori\\db\\{year}\\{mont}\\{nombre_generado}\\'
 
 try:
-    # Verificar que el archivo de origen existe
-    if not os.path.exists(origen):
-        print(f'El archivo de origen no existe: {origen}')
-    else:
-        # Crear la carpeta de destino si no existe
-        os.makedirs(os.path.dirname(destino), exist_ok=True)
+    for origen in archivos_origen:
+        # Verificar que el archivo de origen existe
+        if not os.path.exists(origen):
+            print(f'El archivo de origen no existe: {origen}')
+        else:
+            # Obtener el nombre del archivo
+            nombre_archivo = os.path.basename(origen)
+            # Construir la ruta completa del archivo de destino
+            destino = os.path.join(destino_base, nombre_archivo)
+            
+            # Crear la carpeta de destino si no existe
+            os.makedirs(os.path.dirname(destino), exist_ok=True)
 
-        # Mover el archivo
-        shutil.move(origen, destino)
-        print('Archivo movido con exito.')
+            # Mover el archivo
+            shutil.move(origen, destino)
+            print(f'Archivo {nombre_archivo} movido con éxito.')
 except Exception as e:
-    print(f'Ocurrio un error al mover el archivo: {e}')
+    print(f'Ocurrió un error al mover el archivo: {e}')
